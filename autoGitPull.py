0726dcf63c6da83f261adc9394ccd76335eb02da
liximing1013 <973878442@qq.com>
2021-02-23 18:40:17 +0800

    '修改bug'

--- autoGitPull.py
+++ autoGitPull.py
@@ -1,6 +1,7 @@
 import os
 import git
 from git.repo import Repo
+import time
 
 def autoGitPull2():
     os.popen('cd /home/webroot/banban')
@@ -14,10 +15,11 @@ def autoGitPull():
     g.pull()
     repo = Repo(git_dir)
     commit_log = repo.git.log('--pretty={"commit":"%h","author":"%an","summary":"%s","date":"%cd"}',
-                              max_count=3, date='format:%Y-%m-%d %H:%M')
+                              max_count=3, date='format:%Y-%m-%d %H:%M:%S')
     log_list = commit_log.split("\n")
     real_log_list = [eval(item) for item in log_list][0]['date']
-    print(real_log_list)
+    times = int(time.mktime(real_log_list))
+    print(times)
 
 
 if __name__=="__main__":
