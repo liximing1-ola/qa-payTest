cff49e4bbd554929a1098b820b7cf1ae1ec4b7bc
liximing1013 <973878442@qq.com>
2021-02-23 18:18:21 +0800

    '修改bug'

--- autoGitPull.py
+++ autoGitPull.py
@@ -14,7 +14,7 @@ def autoGitPull():
     g.pull()
     repo = Repo(git_dir)
     commit_log = repo.git.log('--pretty={"commit":"%h","author":"%an","summary":"%s","date":"%cd"}',
-                              max_count=50, date='format:%Y-%m-%d %H:%M')
+                              max_count=3, date='format:%Y-%m-%d %H:%M')
     log_list = commit_log.split("\n")
     real_log_list = [eval(item) for item in log_list]
     print(real_log_list)
