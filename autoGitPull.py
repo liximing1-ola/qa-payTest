17261b40c3a53420fd006d2b51009a48343778bc
liximing1013 <973878442@qq.com>
2021-02-23 18:36:15 +0800

    '修改bug'

--- autoGitPull.py
+++ autoGitPull.py
@@ -16,7 +16,7 @@ def autoGitPull():
     commit_log = repo.git.log('--pretty={"commit":"%h","author":"%an","summary":"%s","date":"%cd"}',
                               max_count=3, date='format:%Y-%m-%d %H:%M')
     log_list = commit_log.split("\n")
-    real_log_list = [eval(item) for item in log_list]
+    real_log_list = [eval(item) for item in log_list][0]['data']
     print(real_log_list)
 
 
---
+++ autoGitPull.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cd /home/webroot/banban && git pull
