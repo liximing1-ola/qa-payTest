291d802cb468f4c312511f8e7a6af8129ecc2aea
liximing1013 <973878442@qq.com>
2021-02-24 11:26:17 +0800

    '修改bug'

--- autoGitPull.py
+++ autoGitPull.py
@@ -12,20 +12,20 @@ def autoGitPull():
         commit_log = repo.git.log('--pretty={"commit":"%h","author":"%an","summary":"%s","date":"%cd"}',
                                   max_count=3, date='format:%Y-%m-%d %H:%M:%S')
         log_list = commit_log.split("\n")
-        logs.get_log('autoGitPull.log').error('当前分支: {}, 最新一条commit: {}'.format(repo.active_branch, log_list[0]))
+        logs.get_log('GitPull.log').info('当前分支: {}, 最新一条commit: {}'.format(repo.active_branch, log_list[0]))
         real_time = [eval(item) for item in log_list][0]['date']
         timeArray = time.strptime(real_time, "%Y-%m-%d %H:%M:%S")
         times = int(time.mktime(timeArray))
         lastTime = int(readUpdateTime())
         if times > lastTime:
-            logs.get_log('gitCode.log').error('最新代码提交时间: {}, 上次代码更新时间: {}'.format(times, lastTime))
+            logs.get_log('updateGitCode.log').error('最新代码提交时间: {}, 上次代码更新时间: {}'.format(times, lastTime))
             return True
         else:
-            logs.get_log('autoGitPull.log').error("Git未拉取到最新代码： {}".format(time.strftime('%F %H:%M',
+            logs.get_log('GitNotPull.log').error("Git未拉取到最新代码： {}".format(time.strftime('%F %H:%M',
                                                                                          time.localtime(time.time()))))
             return False
     else:
-        logs.get_log('autoGitPull.log').error("Git分支不对： {}".format(repo.active_branch))
+        logs.get_log('GitError.log').error("Git分支不对： {}".format(repo.active_branch))
         return False
 
 def writeUpdateTime(now):
