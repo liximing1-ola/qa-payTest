812e21038c7d2a9de9e312e4b4906111d5d0aca7
liximing1013 <973878442@qq.com>
2021-02-25 17:30:18 +0800

    '提交修改'

--- run_all_case.py
+++ run_all_case.py
@@ -10,7 +10,7 @@ def all_case():
     # case_dir = os.path.join(os.getcwd(), "Case")   # 待执行用例的目录
     testcase = unittest.TestSuite()
     discover = unittest.defaultTestLoader.discover("/home/banban-1/payTest/Case",
-                                                   pattern="test_*.py",
+                                                   pattern="test_pay_defend.py",
                                                    top_level_dir=None)
     testcase.addTests(discover)  # 直接加载 discover
     return testcase
@@ -26,17 +26,17 @@ def main():
             logs.get_log('failCase.log').error(test_result.failures)
         for case, reason in test_result.failures:
             if len(test_result.failures) > 0:
-                robot(case.id())
+                robot(case.id(), test_result.failures)
                 break
     else:
         pass
 
 
-def robot(des, ):
+def robot(case, des):
     url = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=f9d916cb-6b93-4389-8aa4-f51c755faa0e'
     headers = {'Content-Type': 'application/json'}
     now = time.strftime('%F:%H:%M', time.localtime(time.time()))
-    title = "警告!内测支付异常__{}".format(now)
+    title = "警告!-{}".format(case)
     des = des
     icon = getImage()
     data = {
