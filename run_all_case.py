27ce9290875046e40d5f77b2ace8f9b935e0978f
liximing1013 <973878442@qq.com>
2021-03-01 11:04:20 +0800

    '提交修改'

--- run_all_case.py
+++ run_all_case.py
@@ -5,6 +5,7 @@ from Common import logs
 from autoGitPull import writeUpdateTime
 from autoGitPull import autoGitPull
 from robot import roBot, roBOt
+
 def all_case():
     # case_dir = os.path.join(os.getcwd(), "Case")   # 待执行用例的目录
     testcase = unittest.TestSuite()
@@ -18,15 +19,14 @@ def main():
     if autoGitPull():
         test_result = unittest.TextTestRunner(verbosity=2).run(all_case())
         writeUpdateTime(str(int(time.time())))
-        now = time.strftime('%F-%H:%M', time.localtime(time.time()))
-        logs.get_log('runCaseTime.log').info("执行用例总数: {}, 失败用例总数: {}, 执行时间: {}"
-                                             .format(test_result.testsRun, len(test_result.failures), now))
-        if len(test_result.failures) == 0:
+        des = "执行用例总数: {}, 失败用例总数: {}, 异常用例总数: {}" \
+            .format(test_result.testsRun, len(test_result.failures), len(test_result.errors))
+        logs.get_log('runCaseTime.log').info(des)
+        if len(test_result.failures) == 0 and len(test_result.errors) == 0:
             time.sleep(5)
-            des = "执行用例总数: {}, 失败用例总数: {}".format(test_result.testsRun, len(test_result.failures))
             roBOt(des)
-        if len(test_result.failures) >= 1:
-            logs.get_log('failCase.log').error(test_result.failures)
+        if len(test_result.failures) >= 1 or len(test_result.errors) >= 1:
+            logs.get_log('failCase.log').error("error: {}, failures: {}".format(test_result.errors, test_result.failures))
         for case, reason in test_result.failures:
             if len(test_result.failures) > 0:
                 roBot(case.id())
