bd3ec9d35294c50c0670ba6e4496a278eee67730
liximing1013 <973878442@qq.com>
2021-03-01 18:56:38 +0800

    '提交修改'

--- run_all_case.py
+++ run_all_case.py
@@ -18,29 +18,30 @@ def all_case():
 
 def main():
     if autoGitPull():
-        html_path = '/home/banban-1/payTest/report/result.html'
-        fp = open(html_path, 'wb')
-        runner = HTMLTestRunner(stream=fp, title=u"测试报告", description=u"用例测试情况")
-        test_result = runner.run(all_case())
-        # test_result = unittest.TextTestRunner(verbosity=2).run(all_case())
+        # 生成HTML格式
+        # html_path = '/home/banban-1/payTest/report/result.html'
+        # fp = open(html_path, 'wb')
+        # runner = HTMLTestRunner(stream=fp, title=u"测试报告", description=u"用例测试情况")
+        # test_result = runner.run(all_case())
+        test_result = unittest.TextTestRunner(verbosity=3).run(all_case())
         writeUpdateTime(str(int(time.time())))
         des = "执行用例总数: {}, 失败用例总数: {}, 异常用例总数: {}" \
             .format(test_result.testsRun, len(test_result.failures), len(test_result.errors))
         logs.get_log('runCaseTime.log').info(des)
         if len(test_result.failures) == 0 and len(test_result.errors) == 0:
             time.sleep(3)
-            #robot_success(des)
+            robot_success(des)
         if len(test_result.failures) >= 1 or len(test_result.errors) >= 1:
             logs.get_log('failCase.log').error("error: {}, failures: {}".format(test_result.errors, test_result.failures))
             time.sleep(3)
-            #robot_success(des)
+            robot_success(des)
         for case, reason in test_result.failures:
             if len(test_result.failures) > 0:
                 roBot(case.id())
                 break
         for case, reason in test_result.errors:
             if len(test_result.errors) > 0:
-                #roBot(case.id())
+                roBot(case.id())
                 break
     else:
         pass
