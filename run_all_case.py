f23ef649c97c7cd13636992f97d4df4204418575
liximing1013 <973878442@qq.com>
2021-03-08 21:39:20 +0800

    '提交优化'

--- run_all_case.py
+++ run_all_case.py
@@ -14,7 +14,7 @@ def all_case():
     testcase = unittest.TestSuite()
     # 指定待执行用例的目录
     discover = unittest.defaultTestLoader.discover("/home/banban-1/payTest/Case",
-                                                   pattern="test_*",
+                                                   pattern="test_pay_shopPay.py",
                                                    top_level_dir=None)
     testcase.addTests(discover)  # 直接加载 discover
     return testcase
