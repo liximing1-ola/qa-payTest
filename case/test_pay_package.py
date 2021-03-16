766113ef596d0b3aa18bd1596e3af42fb2e94e06
liximing1013 <973878442@qq.com>
2021-03-16 17:08:10 +0800

    ‘提交修改’

--- case/test_pay_package.py
+++ case/test_pay_package.py
@@ -172,6 +172,7 @@ class TestPayCreate(unittest.TestCase):
         4.检查被打赏者余额和账户，预期为：3000 * 0.62 = 1860
         5.检查打赏者余额,预期为：3000 -2500 = 500
         """
+        Mysql.deleteUserCommoditySql(config.payUid, 17)
         Mysql.insertXsUserCommodity(config.payUid, 54, 1, 1)
         Mysql.updateMoneySql(3000, 0, 0, 0, config.payUid)
         Mysql.updateMoneySql(0, 0, 0, 0, config.testUid)
