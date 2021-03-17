c9a05c57faca5775be5059894783e3e801b62126
liximing1013 <973878442@qq.com>
2021-03-17 16:21:09 +0800

    ‘提交修改’

--- run_all_case.py
+++ run_all_case.py
@@ -24,8 +24,8 @@ def main():
         # fp = open(html_path, 'wb')
         # runner = HTMLTestRunner(stream=fp, title=u"测试报告", description=u"用例测试情况")
         # test_result = runner.run(all_case())
-        writeUpdateTime(str(int(time.time())))
         test_result = unittest.TextTestRunner(verbosity=3).run(all_case())
+        writeUpdateTime(str(int(time.time())))
         des = "执行用例总数: {}, 失败用例总数: {}, 异常用例总数: {}" \
             .format(test_result.testsRun, len(test_result.failures), len(test_result.errors))
         Logs.get_log('runCaseTime.log').info(des)
