a637829f0965956aa1eec374c89c4f115fd640e1
liximing1013 <973878442@qq.com>
2021-03-18 14:20:27 +0800

    ‘提交修改’

--- common/Config.py
+++ common/Config.py
@@ -5,11 +5,10 @@ class config:
     #  工程目录
     BASE_PATH = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
     #  release域名
-    domain_host = {'bb': 'https://api.yinjietd.com/', 'pt': 'https://api.partying.sg/',
-                   'bb_dev': 'https://dev.iambanban.com/', }
+    domain_host = {'bb': 'https://api.yinjietd.com/', 'bb_dev_58': 'https://dev.iambanban.com/', }
     release_host = domain_host['bb']
     # dev域名
-    dev_host = domain_host['bb_dev']
+    dev_host = domain_host['bb_dev_58']
     # qq_login url
     qq_login_url = dev_host + 'account/qqlogin'
     # 被打赏者
