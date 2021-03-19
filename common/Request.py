3fb80e7496c9d01376c20d37f83d749dcffef728
liximing1013 <973878442@qq.com>
2021-03-19 19:18:22 +0800

    ‘提交修改’

--- common/Request.py
+++ common/Request.py
@@ -20,6 +20,7 @@ def post_request_session(url, data):
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko)\
                             Chrome/67.0.3396.99 Safari/537.36",
         "Content-Type": "application/x-www-form-urlencoded",
+        'Connection': 'close',
         "user-token": get_session['token']
     }
     if not url.startswith('https://'):
