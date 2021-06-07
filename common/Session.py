322f323eba0069650fb27e11ba112953d3d737ad
liximing1013 <973878442@qq.com>
2021-06-07 11:16:53 +0800

    ‘提交修改’

--- common/Session.py
+++ common/Session.py
@@ -35,8 +35,8 @@ class Session:
                 if res['success'] != 1:
                     # Logs.get_log('getSession.log').error('session获取异常，原因： {}'.format(res))
                     raise res['msg']
-                if method.isExtend(res, 'token'):
-                    raise res['msg']
+                #if method.isExtend(res, 'token'):
+                #    raise res['msg']
                 tokenDict = {'token': res['data'].get('token'), 'uid': res['data']['uid']}
                 return tokenDict
             except Exception as error:
@@ -49,11 +49,10 @@ class Session:
                 res = session.post(config.mobile_login_url, data=body, headers=headers)
                 res.raise_for_status()
                 res = res.json()
-                print(res)
                 if res['success'] != 1:
                     raise res['msg']
-                if method.isExtend(res, 'token'):
-                    raise res['msg']
+                #if method.isExtend(res, 'token'):
+                #    raise res['msg']
                 tokenDict = {'token': res['data'].get('token'), 'uid': res['data']['uid']}
                 return tokenDict
             except Exception as error:
