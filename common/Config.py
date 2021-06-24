42c3760c5ee244036da9af51ff0cb52aeb07b042
liximing1013 <973878442@qq.com>
2021-06-24 12:03:07 +0800

    ‘提交修改’

--- common/Config.py
+++ common/Config.py
@@ -8,7 +8,7 @@ class config:
                    'pt': 'https://test.overseaban.com/', }
 
     # 伴伴
-    bb_test = {'payUid': 103273407, 'testUid': 105002312, 'pack_cal_uid': 105002313, 'bb_git_branch': 'release-for-vpc'}
+    bb_test = {'payUid': 103273407, 'testUid': 105002312, 'pack_cal_uid': 105002313, 'bb_git_branch': 'alpha-for-vpc'}
     # dev域名（qq登陆方式)
     dev_host = domain_host['bb_dev_58']
     qq_login_url = dev_host + 'account/qqlogin'
