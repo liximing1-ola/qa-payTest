ffdf18246e82c3440c63703fc03c8ebe48e554ae
liximing1013 <973878442@qq.com>
2021-06-28 18:06:53 +0800

    ‘提交修改’

--- bot_tea.py
+++ bot_tea.py
@@ -1,10 +1,28 @@
 import requests
+import time
 
 def robot():
     url1 = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=e317861a-d1ec-4ac4-af96-9d4b8f12d9d6'
     url= 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=0179d8d1-2078-41ba-a8da-0fb11bd51880'
     headers = {'Content-Type': 'application/json'}
-    reason = {'HighTea-1': 'cake（手快有手慢无系列）', 'HighTea-2': 'milky tea（手快有手慢无系列）', 'HighTea-3': 'cook（无图）', 'HighTea-4': 'chicken（无图）', 'HighTea-5': 'fruit（无图）'}
+    reason = {'HighTea-1': 'cake（手快有手慢无）', 'HighTea-2': 'milky tea（手快有手慢无）', 'HighTea-3': 'cook（无图）', 'HighTea-4': 'chicken（无图）', 'HighTea-5': 'fruit（无图）'}
+    now = time.strftime('%H:%M', time.localtime(time.time()))
+    title = "广告位招租:当前时间-{}, 放松下，自取下午茶".format(now)
+    des = '手快有手慢无'
+    data = {
+        "msgtype": "news",
+        "news": {
+            "articles": [{
+                "title": title,
+                "description": des,
+                "url": "https://meican.com/",
+                "picurl": '/home/banban-1/payTest/001.jpg', }
+            ]
+        }
+    }
+    r = requests.post(
+        url,
+        headers=headers, json=data)
     data = {
         "msgtype": "markdown",
         "markdown": {
