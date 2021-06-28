8761bbf8dc8435f7fb8827335ab2d84e3caea20c
liximing1013 <973878442@qq.com>
2021-06-28 17:51:32 +0800

    ‘提交修改’

--- bot_tea.py
+++ bot_tea.py
@@ -1,5 +1,4 @@
 import requests
-from common.method import dictToList
 
 def robot():
     url1 = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=e317861a-d1ec-4ac4-af96-9d4b8f12d9d6'
@@ -12,7 +11,25 @@ def robot():
             "content": dictToList(reason)
         }
     }
-    requests.post(url, headers=headers, json=data)
+    r = requests.post(
+        url,
+        headers=headers, json=data)
+    if r.status_code == 200 and r.text.find('ok'):
+        data = {
+            "msgtype": "text",
+            "text": {
+                "mentioned_mobile_list": ["@all"]
+            }
+        }
+        requests.post(url, headers=headers, json=data)
+
+def dictToList(result_dict):
+    list_case = []
+    for k, v in result_dict.items():
+        list_case.append(
+            '<font color="comment">{}</font>,  套餐: <font color=\"info\">{}</font>'.format(k, v))
+    case = '\n'.join(list_case)
+    return case
 
 
 if __name__ == '__main__':
