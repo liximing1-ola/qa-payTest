05a28671b3548b170d1e3fda1b2527ed6a849920
liximing1013 <973878442@qq.com>
2021-07-08 17:17:34 +0800

    ‘提交修改’

--- case/test_pay_bean.py
+++ case/test_pay_bean.py
@@ -183,6 +183,7 @@ class TestPayCreate(unittest.TestCase):
         data = Yaml.read_yaml('Basic.yml', 'dev_gold_Combo')
         res = Request.post_request_session(url=TestPayCreate.pay_url, data=data)
         reason = 'Depiction: {},  failReason: {}'.format(des, res['body'])
+        print(Mysql.selectAllMoneySql(config.payUid))
         Assert.assert_code(res['code'], 200)
         Assert.assert_body(res['body'], 'success', 1, reason)
         Assert.assert_equal(Mysql.selectMoneySql(config.payUid), 100)
