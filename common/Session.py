b43ed204fffbc796f2a80171913f203843ff1cb0
liximing1013 <973878442@qq.com>
2021-07-21 23:40:07 +0800

    ‘提交修改’

--- common/Session.py
+++ common/Session.py
@@ -29,10 +29,10 @@ class Session:
                 body = Yaml.read_yaml('Basic.yml', 'data_dev_qq')
                 session = requests.session()
                 res = session.post(login_url, data=body, headers=headers)
-                print(res.json())
                 res.raise_for_status()
                 res = res.json()
                 if not method.isExtend(res, 'token') or res['success'] != 1:
+                    print(1)
                     return res['msg']
                 tokenDict = {'token': res['data'].get('token'), 'uid': res['data']['uid']}
                 return tokenDict
