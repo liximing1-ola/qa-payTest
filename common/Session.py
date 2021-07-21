d3135c777d1024eee1920c2a5637c11f628737cf
liximing1013 <973878442@qq.com>
2021-07-21 23:42:26 +0800

    ‘提交修改’

--- common/Session.py
+++ common/Session.py
@@ -33,6 +33,7 @@ class Session:
                 res = res.json()
                 if not method.isExtend(res, 'token') or res['success'] != 1:
                     print(1)
+                    print(res['msg'])
                     return res['msg']
                 tokenDict = {'token': res['data'].get('token'), 'uid': res['data']['uid']}
                 return tokenDict
