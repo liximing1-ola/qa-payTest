7f1697e03cd497ce4b7854db830f82206a79fafc
liximing1013 <973878442@qq.com>
2021-08-17 10:44:49 +0800

    ‘提交修改’

--- case/test_pay_defend.py
+++ case/test_pay_defend.py
@@ -17,14 +17,14 @@ class TestPayCreate(unittest.TestCase):
         开通个人守护，收益分成在师父收益的基础上为 62:38
         脚本步骤：
         1.构造开通者和被守护者数据 （更新xs_user_money）
-        2.开通价值52守护
+        2.开通价值5200元守护
         3.校验【status code】和返回值【body】状态
         4.检查打赏者余额
-        5.检查被打赏者余额,预期：5200 * 0.62 = 3224
+        5.检查被打赏者余额,预期：520000 * 0.62 = 322400
         6.检查消费记录
         """
         des = '开通个人守护的场景'
-        Mysql.updateMoneySql(config.payUid, 5200)
+        Mysql.updateMoneySql(config.payUid, 520000)
         Mysql.updateMoneySql(config.testUid)
         data = Yaml.read_yaml('Basic.yml', 'dev_pay_defend')
         res = Request.post_request_session(url=TestPayCreate.pay_url, data=data)
@@ -32,8 +32,8 @@ class TestPayCreate(unittest.TestCase):
         Assert.assert_code(res['code'], 200)
         Assert.assert_body(res['body'], 'success', 1, reason)
         Assert.assert_equal(Mysql.selectAllMoneySql(config.payUid), 0)
-        Assert.assert_equal(Mysql.selectAllMoneySql(config.testUid), 3224)
-        Assert.assert_equal(Mysql.selectPayChangeSql(config.payUid), 5200)
+        Assert.assert_equal(Mysql.selectAllMoneySql(config.testUid), 322400)
+        Assert.assert_equal(Mysql.selectPayChangeSql(config.payUid), 520000)
         Assert.assert_equal(Mysql.selectPayChangeOpSql(config.payUid), 'consume')
         Consts.CASE_LIST[des] = 'pass'
 
