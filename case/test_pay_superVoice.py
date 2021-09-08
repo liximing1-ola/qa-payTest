363ead2327d6d9af2dc5e1c821690f78a0489685
liximing1013 <973878442@qq.com>
2021-09-08 15:04:54 +0800

    ‘提交修改’

--- case/test_pay_superVoice.py
+++ case/test_pay_superVoice.py
@@ -37,7 +37,7 @@ class TestPayCreate(unittest.TestCase):
         3.校验【status code】和返回值【body】状态
         4.检查被打赏者余额，预期为：350(个人魅力值)
         """
-        des = '网赚35'
+        des = '网赚房无公会无经纪人艺人收35个人魅力值'
         Mysql.updateMoneySql(config.payUid, 1000)
         test_uid=config.super_live_role['testUid']
         Mysql.updateMoneySql(test_uid)
@@ -60,7 +60,7 @@ class TestPayCreate(unittest.TestCase):
         3.校验【status code】和返回值【body】状态
         4.检查被打赏者余额，预期为：350（公会魅力值）
         """
-        des = '无经纪人35'
+        des = '网赚房指定工会无经纪人艺人收35工会魅力值'
         Mysql.updateMoneySql(config.payUid, 1000)
         test_uid = config.super_live_role['super_star_uid']
         test_bid = config.super_live_role['super_broker']
@@ -87,7 +87,7 @@ class TestPayCreate(unittest.TestCase):
         4.检查被打赏者余额，预期为：500（工会魅力值）
         5.检查经纪人余额，预期为：150（工会魅力值）
         """
-        des = '公会5015'
+        des = '网赚房指定工会有经纪人(1级)艺人分成50:15'
         Mysql.updateMoneySql(config.payUid, 1000)
         test_uid = config.super_live_role['agent_star_uid']
         test_bid = config.super_live_role['super_broker']
@@ -121,7 +121,7 @@ class TestPayCreate(unittest.TestCase):
         4.检查被打赏者余额，预期为：500
         5.检查经纪人余额，预期为：200
         """
-        des = '公会5020'
+        des = '网赚房指定工会有经纪人(7级)的艺人分成50:15'
         Mysql.updateMoneySql(config.payUid, 1000)
         test_uid = config.super_live_role['agent_star_uid']
         test_bid = config.super_live_role['super_broker']
@@ -154,7 +154,7 @@ class TestPayCreate(unittest.TestCase):
         3.校验【status code】和返回值【body】状态
         4.检查被打赏者余额，预期为：500
         """
-        des = '普通5020'
+        des = '网赚房普通工会有经纪人(7j)艺人'
         Mysql.updateMoneySql(config.payUid, 1000)
         test_uid = config.super_live_role['pack_cal_uid']
         test_bid = config.super_live_role['super_broker']
