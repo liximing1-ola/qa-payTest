b123f8ab9fc7541b5f0ff907d45729e98e30678c
liximing1013 <973878442@qq.com>
2021-09-14 17:19:09 +0800

    ‘提交修改’

--- run_all_case.py
+++ run_all_case.py
@@ -15,7 +15,7 @@ def all_case():
     testcase = unittest.TestSuite()
     # 指定待执行用例的目录
     discover = unittest.defaultTestLoader.discover(case_dir['bb_dir'],
-                                                   pattern="test_*",
+                                                   pattern="test_pay_livePackage.py",
                                                    top_level_dir=None)
     testcase.addTests(discover)
     return testcase
