36dc0272009f3065b3e6cf7af1d36a38edee4afc
liximing1013 <973878442@qq.com>
2021-09-15 18:02:19 +0800

    ‘提交修改’

--- run_crontab_case.py
+++ run_crontab_case.py
@@ -16,7 +16,7 @@ def main():
     test_result = unittest.TextTestRunner(verbosity=3).run(all_case())
     des = "定时任务执行数: {}, 失败用例数: {}, 异常用例数: {}".format(test_result.testsRun, len(test_result.failures), len(test_result.errors))
     Logs.get_log('caseResult_2.log').info(des)
-    now = time.strftime('%m-%d %H:%M', time.localtime(time.time()))
+    now = time.strftime('%Y/%m/%d-%H:%M', time.localtime(time.time()))
     if len(test_result.failures) == 0 and len(test_result.errors) == 0:
         des = "时间：{}，用例数: {}, 失败数: {}, 分支：{}".format(now, test_result.testsRun,
                                                                        len(test_result.failures) + len(test_result.errors),
