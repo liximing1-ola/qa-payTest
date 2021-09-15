541409f30798cd14d5271468b70f2ed3c8d3f273
liximing1013 <973878442@qq.com>
2021-09-15 19:21:42 +0800

    ‘提交修改’

--- run_crontab_case.py
+++ run_crontab_case.py
@@ -25,7 +25,7 @@ def main():
     elif len(test_result.failures) >= 1:
         Logs.get_log('failCase.log').error("failures: {}".format(test_result.failures))
         for case, reason in test_result.failures:
-            print(case.id().strip('.'))
+            print(case.id().split('.'))
             robot('icon', case.id(), bot='test')
     elif len(test_result.errors) >= 1:
         Logs.get_log('failCase.log').error("error: {}".format(test_result.errors))
