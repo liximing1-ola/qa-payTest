5458b9f2dcdefa8dd3ff99f179e48d0f4a4c3813
liximing1013 <973878442@qq.com>
2021-09-15 18:48:49 +0800

    ‘提交修改’

--- run_crontab_case.py
+++ run_crontab_case.py
@@ -14,9 +14,9 @@ def all_case():
 
 def main():
     test_result = unittest.TextTestRunner(verbosity=3).run(all_case())
-    des = "定时任务执行数: {}, 失败用例数: {}, 异常用例数: {}".format(test_result.testsRun, len(test_result.failures), len(test_result.errors))
-    Logs.get_log('caseResult_2.log').info(des)
     now = time.strftime('%Y-%m-%d %H:%M', time.localtime(time.time()))
+    des = "{},定时任务执行数: {}, 失败用例数: {}, 异常用例数: {}".format(now, test_result.testsRun, len(test_result.failures), len(test_result.errors))
+    Logs.get_log('caseResult_2.log').info(des)
     if len(test_result.failures) == 0 and len(test_result.errors) == 0:
         des = "{}, 用例数:{}, 失败数:{}, 分支:{}".format(now, test_result.testsRun,
                                                                        len(test_result.failures) + len(test_result.errors),
@@ -24,16 +24,12 @@ def main():
         robot('markdown', des, bot='test')
     elif len(test_result.failures) >= 1:
         Logs.get_log('failCase.log').error("failures: {}".format(test_result.failures))
-        time.sleep(0.2)
         for case, reason in test_result.failures:
             robot('icon', Consts.fail_case_reason, title=case.id(), bot='test')
     elif len(test_result.errors) >= 1:
         Logs.get_log('failCase.log').error("error: {}".format(test_result.errors))
-        time.sleep(0.2)
         for case, reason in test_result.errors:
             robot('icon', Consts.fail_case_reason, title=case.id(), bot='test')
-
-
 
 
 if __name__ == "__main__":
