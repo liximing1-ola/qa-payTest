92d179b785c4469c0658b52f0207f5774d6b2f81
liximing1013 <973878442@qq.com>
2021-10-12 18:02:28 +0800

    ‘提交修改’

--- case/test_pay_defend.py
+++ case/test_pay_defend.py
@@ -2,7 +2,6 @@ from common.Config import config
 import unittest
 from common import Consts, Assert, basicData, Request
 from common.runFailed import Retry
-# from common.conMysql import Mysql
 from common.sqlScript import Mysql
 @Retry(max_n=3)
 class TestPayCreate(unittest.TestCase):
