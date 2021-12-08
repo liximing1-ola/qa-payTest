305b557c554453badb61cfe50e50d5baa63dafd2
liximing1013 <973878442@qq.com>
2021-12-08 18:37:34 +0800

    ‘提交修改’

--- probabilityTest/gift.py
+++ probabilityTest/gift.py
@@ -41,8 +41,8 @@ def postPayCreate_gift(num):
         }
         d = urllib.parse.urlencode(data)
         data = d.replace('+', '').replace('%27', '%22')
+        time.sleep(5)
         res = requests.post(url, data=data, headers=headers)
-        time.sleep(2)
         res = res.json()
         if res['success'] == 1:
             pass
