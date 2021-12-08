f8bdedb3c0763fd7d25212526341d600361f86f9
liximing1013 <973878442@qq.com>
2021-12-08 18:09:11 +0800

    ‘提交修改’

--- probabilityTest/gift.py
+++ probabilityTest/gift.py
@@ -4,7 +4,7 @@ import urllib.parse
 import pymysql
 import random
 import time
-def postPayCreate_gift():
+def postPayCreate_gift(num):
     url = "https://dev.iambanban.com/pay/create?package=com.imbb.banban.android"
     gift_dict = checkGiftSql()
     print(gift_dict)
@@ -12,42 +12,44 @@ def postPayCreate_gift():
         'Content-Type': "application/x-www-form-urlencoded",
         'cache-control': "no-cache",
         'Postman-Token': "f7d705b2-cf29-4a4a-81ba-2c8c8d0f5ed5",
-        "user-token": 'd3ccnXDBWkTkXLbr8PuGJegJdEPc6x9Sc__2BW32yxxuOWuNEfsEaHU1o4oKXtPNH9tylxUBv4Tt855126jdSUuZQ0eLMp__2BVLyltuTqHGSas20dOBF6__2FxPn7hc6'}
+        "user-token": '0ee5fmt4__2FI5C4hAjxM8QqGmrLVQpzsrNdu1fKNQyGDBj9pXxNpQrvgsBJdddWFiAVwRZ__2BTlFv7C4O6sq0y6Yf5WjdxRKNsJp63WGbfYpbajc3mkftXHpJznn'}
     for i in gift_dict:
-        data = {
-            "platform": "available",
-            "type": "package",
-            "money": v*5,
-            "params":
-                {"rid": 200000934,
-                 "uids": "100287189,100010150,100010151,100010152,100010153",
-                 "positions": "0,1,2,3,4",
-                 "position": -1,
-                 "giftId": k,
-                 "giftNum": 1,
-                 "price": v,
-                 "cid": 0,
-                 "ctype": "",
-                 "duction_money": 0,
-                 "version": 2,
-                 "num": 5,
-                 "gift_type": 'normal',
-                 "useCoin": -1,
-                 "star": 0,
-                 "show_pac_man_guide": 1,
-                 "refer": "",
-                 "all_mic": 0,
-                 }
-        }
-        d = urllib.parse.urlencode(data)
-        data = d.replace('+', '').replace('%27', '%22')
-        res = requests.post(url, data=data, headers=headers)
-        time.sleep(2)
-        res = res.json()
-        if res['success'] == 1:
-            pass
-        else:
-            raise EnvironmentError(res)
+        for j in i:
+            data = {
+                "platform": "available",
+                "type": "package",
+                "money": int(j[1]) * 100 * num,
+                "params":
+                    {"rid": 200000930,
+                     "uids": "105002315",
+                     "positions": "0",
+                     "position": -1,
+                     "giftId": j[0],
+                     "giftNum": num,
+                     "price": int(j[1]) * 100,
+                     "cid": 0,
+                     "ctype": "",
+                     "duction_money": 0,
+                     "version": 2,
+                     "num": 5,
+                     "gift_type": 'normal',
+                     "useCoin": -1,
+                     "star": 0,
+                     "show_pac_man_guide": 1,
+                     "refer": "",
+                     "all_mic": 0,
+                     }
+            }
+            d = urllib.parse.urlencode(data)
+            data = d.replace('+', '').replace('%27', '%22')
+            res = requests.post(url, data=data, headers=headers)
+            time.sleep(2)
+            res = res.json()
+            if res['success'] == 1:
+                pass
+            else:
+                raise EnvironmentError(res)
+
 
 def conMysql():
     db_config = {"dev_46_db": '192.168.11.46',
@@ -63,9 +65,9 @@ def conMysql():
     cursor = con.cursor()
     return con, cursor
 
-def updateBeanSql(uid, coupon_money):
+def updateMoneySql(uid, money):
     con, cur = conMysql()
-    sql = "update xs_user_money_extend set money_coupon={} where uid={} limit 1".format(coupon_money, uid)
+    sql = "update xs_user_money set money{} where uid={} limit 1".format(money, uid)
     try:
         cur.execute(sql)
     except Exception as error:
@@ -92,18 +94,14 @@ def checkGiftSql():
         time.sleep(0.1)
         con.commit()
 
-
 def main_pay():
     i = 1
-    # updateBeanSql(128440017, coupon_money=1000000000)
-    # updateBeanSql(105002315, 0)
+    updateMoneySql(128440017, money=1000000000)
     while i < 10000:
-        num = int(random.choice('136'))
-        print('第{}次, 开蛋数为{}'.format(i, num))
-        postPayCreate_gift()
+        postPayCreate_gift(1)
         time.sleep(1)
         i += 1
 
 
 if __name__=='__main__':
-    postPayCreate_gift()
+    postPayCreate_gift(1)
