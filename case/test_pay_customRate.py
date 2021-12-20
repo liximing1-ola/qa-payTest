9ea112586976b16c8077e01b5ded39c864f41695
liximing1013 <973878442@qq.com>
2021-12-20 19:17:19 +0800

    ‘提交修改’

--- case/test_pay_customRate.py
+++ case/test_pay_customRate.py
@@ -54,7 +54,7 @@ class TestPayCreate(unittest.TestCase):
         4.检查打赏者余额，预期为：1000 - 1000 = 0
         5.检查被打赏者总余额，预期为：1000 * 0.5 * 0.8 + 1000 * 0.3 = 700
         6.检查被打赏者公会魅力值余额,预期为：1000 * 0.5 * 0.8 = 400（公会魅力值）
-        6.检查被打赏者公会长余额，预期为：1000 * 0.5 *（1-0.8) = 100(公会魅力值)
+        7.检查被打赏者公会长余额，预期为：1000 * 0.5 *（1-0.8) = 100(公会魅力值)
         """
         conMysql.updateMoneySql(config.payUid, money=930, money_cash=30, money_cash_b=30, money_b=10)
         testUid = config.rewardUid2  # 被打赏者
@@ -70,4 +70,87 @@ class TestPayCreate(unittest.TestCase):
         assert_equal(conMysql.selectUserMoneySql('sum_money', testUid), 700)
         assert_equal(conMysql.selectUserMoneySql('single_money', testUid, money_type='money_cash'), 400)
         assert_equal(conMysql.selectUserMoneySql('single_money', ceoUid, money_type='money_cash'), 100)
+        case_list_b[des] = result
+
+    def test_03_defendPayCustomRate_25(self, des='个人守护打赏自定义分成:25'):
+        """
+        用例描述：
+        tdr:后台自定义分成比例为25%（所得公会魅力值部分-70%与公会长按照比例分成）
+        脚本步骤：
+        1.构造打赏者，被打赏者和公会长数据
+        2.开通守护
+        3.校验接口状态和返回值数据
+        4.检查打赏者余额，预期为：52000 - 52000 = 0
+        5.检查被打赏者总余额，预期为：52000 * 0.7 * 0.25 = 9100
+        6.检查被打赏者公会长余额，预期为：52000 * 0.7 *（1-0.25) = 27300
+        """
+        conMysql.updateMoneySql(config.payUid, money=52000)
+        testUid = config.rewardUid2  # 被打赏者
+        ceoUid = config.live_role['pack_ceo']  # 关联公会长
+        conMysql.updateUserMoneyClearSql(testUid, ceoUid)
+        conMysql.checkUserBroker(testUid, bid=ceoUid)  # bid=105002314 被打赏者加入工会
+        conMysql.checkBrokerUserRate(testUid, ceoUid, rate=25)  # config.bbc_broker_user_rate 设置分成比
+        data = basicData.encodeData(payType='defend', uid=testUid, money=52000)
+        res = post_request_session(config.pay_url, data)
+        assert_code(res['code'])
+        assert_body(res['body'], 'success', 1, reason(des, res))
+        assert_equal(conMysql.selectUserMoneySql('sum_money', config.payUid), 0)
+        assert_equal(conMysql.selectUserMoneySql('single_money', testUid, money_type='money_cash'), 9100)
+        assert_equal(conMysql.selectUserMoneySql('single_money', ceoUid, money_type='money_cash'), 27300)
+        case_list_b[des] = result
+
+    @unittest.skip('未完成')
+    def test_04_fleetRoomPayCustomRate_100(self, des='本家族房打赏自定义分成:100'):
+        """
+        用例描述：
+        tdr:后台自定义分成比例为25%（所得公会魅力值部分-70%与公会长按照比例分成）
+        脚本步骤：
+        1.构造打赏者，被打赏者和公会长数据
+        2.开通守护
+        3.校验接口状态和返回值数据
+        4.检查打赏者余额，预期为：52000 - 52000 = 0
+        5.检查被打赏者总余额，预期为：52000 * 0.7 * 0.25 = 9100
+        6.检查被打赏者公会长余额，预期为：52000 * 0.7 *（1-0.25) = 27300
+        """
+        conMysql.updateMoneySql(config.payUid, money=52000)
+        testUid = config.rewardUid2  # 被打赏者
+        ceoUid = config.live_role['pack_ceo']  # 关联公会长
+        conMysql.updateUserMoneyClearSql(testUid, ceoUid)
+        conMysql.checkUserBroker(testUid, bid=ceoUid)  # bid=105002314 被打赏者加入工会
+        conMysql.checkBrokerUserRate(testUid, ceoUid, rate=25)  # config.bbc_broker_user_rate 设置分成比
+        data = basicData.encodeData(payType='defend', uid=testUid, money=52000)
+        res = post_request_session(config.pay_url, data)
+        assert_code(res['code'])
+        assert_body(res['body'], 'success', 1, reason(des, res))
+        assert_equal(conMysql.selectUserMoneySql('sum_money', config.payUid), 0)
+        assert_equal(conMysql.selectUserMoneySql('single_money', testUid, money_type='money_cash'), 9100)
+        assert_equal(conMysql.selectUserMoneySql('single_money', ceoUid, money_type='money_cash'), 27300)
+        case_list_b[des] = result
+
+    @unittest.skip('未完成')
+    def test_05_unFleetRomePayCustomRate_0(self, des='非本家族房打赏自定义分成:0'):
+        """
+        用例描述：
+        tdr:后台自定义分成比例为25%（所得公会魅力值部分-70%与公会长按照比例分成）
+        脚本步骤：
+        1.构造打赏者，被打赏者和公会长数据
+        2.开通守护
+        3.校验接口状态和返回值数据
+        4.检查打赏者余额，预期为：52000 - 52000 = 0
+        5.检查被打赏者总余额，预期为：52000 * 0.7 * 0.25 = 9100
+        6.检查被打赏者公会长余额，预期为：52000 * 0.7 *（1-0.25) = 27300
+        """
+        conMysql.updateMoneySql(config.payUid, money=52000)
+        testUid = config.rewardUid2  # 被打赏者
+        ceoUid = config.live_role['pack_ceo']  # 关联公会长
+        conMysql.updateUserMoneyClearSql(testUid, ceoUid)
+        conMysql.checkUserBroker(testUid, bid=ceoUid)  # bid=105002314 被打赏者加入工会
+        conMysql.checkBrokerUserRate(testUid, ceoUid, rate=25)  # config.bbc_broker_user_rate 设置分成比
+        data = basicData.encodeData(payType='defend', uid=testUid, money=52000)
+        res = post_request_session(config.pay_url, data)
+        assert_code(res['code'])
+        assert_body(res['body'], 'success', 1, reason(des, res))
+        assert_equal(conMysql.selectUserMoneySql('sum_money', config.payUid), 0)
+        assert_equal(conMysql.selectUserMoneySql('single_money', testUid, money_type='money_cash'), 9100)
+        assert_equal(conMysql.selectUserMoneySql('single_money', ceoUid, money_type='money_cash'), 27300)
         case_list_b[des] = result
