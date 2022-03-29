a5cb940b7a36575ccc0b2f151b0bd429d733eef3
liximing1013 <973878442@qq.com>
2022-03-29 17:32:52 +0800

    ‘提交修改’

--- probabilityTest/ferris.py
+++ probabilityTest/ferris.py
@@ -7,16 +7,16 @@ from common.basicData import encodeData
 
 
 def ferrisData():
-    uids = ('131565025', '131564957', '131542117', '128439987', '105002120', '105002231', '131541046', '131541047',
+    uid = ('131565025', '131564957', '131542117', '128439987', '105002120', '105002231', '131541046', '131541047',
             '131542035', '131542036', '131542037', '131542038', '131542039', '131542040')
     headers = {
         'Content-Type': "application/x-www-form-urlencoded",
         'cache-control': "no-cache",
         'Postman-Token': "f7d705b2-cf29-4a4a-81ba-2c8c8d0f5ed5",
         "user-token": '441dYCJG63WGikAgmAg17KtlHsSZyCagZABGIAi3iykaK76GvwGi8xwuFgYdWZLgdup8dOD03bX2HzCbskOoFR2XH6mX5hDuWEJKPXEKKx__2FWbJb5HuSt3U6F'}
-    data = encodeData(payType='package-more', uids=uids, rid='200022564', giftId=5)
+    data = encodeData(payType='package-more', uids=uid, rid='200022564', giftId=5)
     res = requests.post(url=config.pay_url, headers=headers, data=data)
-    print(res.text)
+    print(res.json())
 
 
 if __name__ == '__main__':
