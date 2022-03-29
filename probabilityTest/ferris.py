271c80b13505016730c230e1766961f91ca28724
liximing1013 <973878442@qq.com>
2022-03-29 19:22:17 +0800

    ‘提交修改’

--- probabilityTest/ferris.py
+++ probabilityTest/ferris.py
@@ -12,7 +12,7 @@ def ferrisData():
         'Content-Type': "application/x-www-form-urlencoded",
         'cache-control': "no-cache",
         'Postman-Token': "f7d705b2-cf29-4a4a-81ba-2c8c8d0f5ed5",
-        "user-token": '441dYCJG63WGikAgmAg17KtlHsSZyCagZABGIAi3iykaK76GvwGi8xwuFgYdWZLgdup8dOD03bX2HzCbskOoFR2XH6mX5hDuWEJKPXEKKx__2FWbJb5HuSt3U6F'}
+        "user-token": 'f1e7Y__2F61q5JZ6yGwIfuiqa__2FjoNuMpm__2BumZl0y0__2BkHfTGiG8VNqi7nBlnlqIz__2BitjLGqVUWnFtiVsQ4omz3dRRguKxDUs3GQrAWnk0PxIlnH2VBfFKKJwtPKx'}
     data = encodeData(payType='package-more', uids=uids, rid='200022564', giftId=88, money=52000)
     res = requests.post(url=config.pay_url, headers=headers, data=data)
     print(res.json())
