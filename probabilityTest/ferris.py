248e96a37dea1985e911f2e77554aa330de192ea
liximing1013 <973878442@qq.com>
2022-03-29 18:08:18 +0800

    ‘提交修改’

--- probabilityTest/ferris.py
+++ probabilityTest/ferris.py
@@ -6,8 +6,6 @@ from common.Config import config
 from common.basicData import encodeData
 from common.sqlScript import mysql
 def ferrisData():
-    uid = ('131565025', '131564957', '131542117', '128439987', '105002120', '105002231', '131541046', '131541047',
-            '131542035', '131542036', '131542037', '131542038', '131542039', '131542040')
     uids = mysql.getUids(20)
     print(uids)
     headers = {
@@ -15,7 +13,7 @@ def ferrisData():
         'cache-control': "no-cache",
         'Postman-Token': "f7d705b2-cf29-4a4a-81ba-2c8c8d0f5ed5",
         "user-token": '441dYCJG63WGikAgmAg17KtlHsSZyCagZABGIAi3iykaK76GvwGi8xwuFgYdWZLgdup8dOD03bX2HzCbskOoFR2XH6mX5hDuWEJKPXEKKx__2FWbJb5HuSt3U6F'}
-    data = encodeData(payType='package-more', uids=uids, rid='200022564', giftId=3, money=52000)
+    data = encodeData(payType='package-more', uids=uids, rid='200022564', giftId=88, money=52000)
     res = requests.post(url=config.pay_url, headers=headers, data=data)
     print(res.json())
 
