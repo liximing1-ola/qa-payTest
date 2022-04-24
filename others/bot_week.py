a679329c6cf07721eb41c834b491888355cb6e99
liximing1013 <973878442@qq.com>
2022-04-24 18:11:51 +0800

    ‘提交修改’

--- others/bot_week.py
+++ others/bot_week.py
@@ -4,7 +4,8 @@ def robot():
     url = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=ddf98ffd-fd35-42be-8362-5e485b08226a'  # 质量周报机器人
     headers = {'Content-Type': 'application/json'}
     reason = {'【雷鸟】': '所有的不甘,都是因为还心存梦想,在你放弃之前,好好拼一把,只怕心老,不怕路长'}
-    title = "点击图片查看本期质量周报"
+    now = time.strftime('%m-%d', time.localtime(time.time()))
+    title = "点击图片查看本期{}质量周报".format(now)
     des = ''
     data = {
         "msgtype": "news",
@@ -13,7 +14,7 @@ def robot():
                 "title": title,
                 "description": des,
                 "url": "https://doc.weixin.qq.com/sheet/e3_AHEAsAZ-AB0kquttATbRHC7ipzUTg?scode=ACkAfQfVAAg4ke8jKPAHEAsAZ-AB0",
-                "picurl": 'http://m.qpic.cn/psc?/V52hTYBe40XQKg3ty77Y0YPE4S2QKHbM/ruAMsa53pVQWN7FLK88i5onIdaPw.TVAo3FcNaFagANRdCaeDmBuM.k0g6.otuNRgyMOWe6XE1qOvXwKqlnIWnCrffIdOQ2B5zACbFpMqoI!/b&bo=0AK*AAAAAAADB08!&rf=viewer_4', }
+                "picurl": 'http://m.qpic.cn/psc?/V52hTYBe40XQKg3ty77Y0YPE4S2QKHbM/ruAMsa53pVQWN7FLK88i5onIdaPw.TVAo3FcNaFagAP5X30GQIOF8AtsAK3n6r7ze5o*.QQwxTB2*EobnEj1LsOg*fFXtHIU*finGKUgGgo!/b&bo=tAO*AAAAAAADByo!&rf=viewer_4', }
             ]
         }
     }
