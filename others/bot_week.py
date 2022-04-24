bc88955bb252b6e10aa2bc1abd63bd70a84aa789
liximing1013 <973878442@qq.com>
2022-04-24 18:18:53 +0800

    ‘提交修改’

--- others/bot_week.py
+++ others/bot_week.py
@@ -3,7 +3,7 @@ import time
 def robot():
     url = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=ddf98ffd-fd35-42be-8362-5e485b08226a'  # 质量周报机器人
     headers = {'Content-Type': 'application/json'}
-    reason = {'【雷鸟】': '所有的不甘,都是因为还心存梦想,在你放弃之前,好好拼一把,只怕心老,不怕路长'}
+    # reason = {'【雷鸟】': '所有的不甘,都是因为还心存梦想,在你放弃之前,好好拼一把,只怕心老,不怕路长'}
     now = time.strftime('%m-%d', time.localtime(time.time()))
     title = "查看质量周报_{}".format(now)
     des = '点击图片查看文档'
@@ -16,15 +16,6 @@ def robot():
                 "url": "https://doc.weixin.qq.com/sheet/e3_AHEAsAZ-AB0kquttATbRHC7ipzUTg?scode=ACkAfQfVAAg4ke8jKPAHEAsAZ-AB0",
                 "picurl": 'http://m.qpic.cn/psc?/V52hTYBe40XQKg3ty77Y0YPE4S2QKHbM/ruAMsa53pVQWN7FLK88i5rUbx260PN7cBpa3cHGXHLPsLOcPmWzR41DBbV7JpWN4k7XVVmnnuDdrGo5K*E.oqFOPsoJVxFY6xP9uhukeB1o!/b&bo=0ALzAQAAAAADBwI!&rf=viewer_4', }
             ]
-        }
-    }
-    requests.post(
-        url,
-        headers=headers, json=data)
-    data = {
-        "msgtype": "markdown",
-        "markdown": {
-            "content": dictToList(reason)
         }
     }
     requests.post(
