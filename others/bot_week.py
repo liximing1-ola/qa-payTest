31d30eb6db15eb95745c348caef7a446ba56251a
liximing1013 <973878442@qq.com>
2022-04-24 18:15:50 +0800

    ‘提交修改’

--- others/bot_week.py
+++ others/bot_week.py
@@ -5,8 +5,8 @@ def robot():
     headers = {'Content-Type': 'application/json'}
     reason = {'【雷鸟】': '所有的不甘,都是因为还心存梦想,在你放弃之前,好好拼一把,只怕心老,不怕路长'}
     now = time.strftime('%m-%d', time.localtime(time.time()))
-    title = "点击图片查看本期{}质量周报".format(now)
-    des = ''
+    title = "查看质量周报_{}".format(now)
+    des = '点击图片查看文档'
     data = {
         "msgtype": "news",
         "news": {
@@ -14,7 +14,7 @@ def robot():
                 "title": title,
                 "description": des,
                 "url": "https://doc.weixin.qq.com/sheet/e3_AHEAsAZ-AB0kquttATbRHC7ipzUTg?scode=ACkAfQfVAAg4ke8jKPAHEAsAZ-AB0",
-                "picurl": 'http://m.qpic.cn/psc?/V52hTYBe40XQKg3ty77Y0YPE4S2QKHbM/ruAMsa53pVQWN7FLK88i5onIdaPw.TVAo3FcNaFagAP5X30GQIOF8AtsAK3n6r7ze5o*.QQwxTB2*EobnEj1LsOg*fFXtHIU*finGKUgGgo!/b&bo=tAO*AAAAAAADByo!&rf=viewer_4', }
+                "picurl": 'http://m.qpic.cn/psc?/V52hTYBe40XQKg3ty77Y0YPE4S2QKHbM/ruAMsa53pVQWN7FLK88i5rUbx260PN7cBpa3cHGXHLPsLOcPmWzR41DBbV7JpWN4k7XVVmnnuDdrGo5K*E.oqFOPsoJVxFY6xP9uhukeB1o!/b&bo=0ALzAQAAAAADBwI!&rf=viewer_4', }
             ]
         }
     }
