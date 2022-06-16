ab50bcf9db4f323ac91b8beccfd4ffadaff37e74
wubing <wu.bingbing@olaola.chat>
2022-06-16 19:25:00 +0800

    '摩天轮'

---
+++ common/Greedy.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#  @author: WuBingBing
+
+from common.Config import config
+from common.Request import post_request_session
+from common.conPtMysql import conMysql
+import time
+import random
+
+
+class Greedy:
+
+    @staticmethod
+    def greedy_index(uid):
+        url = 'greedy/index?uid={}'.format(uid)
+        return post_request_session(config.pt_host+url, None, tokenName='pt')
+
+    @staticmethod
+    def greedy_stake(uid, vid, counter, round_id, money_type='gold_coin', notice=False):
+        url = 'greedy/stake?uid={}'.format(uid)
+        params = {
+            'vid': vid,
+            'counter': counter,
+            'round_id': round_id,
+            'money_type': money_type,
+            'notice': notice
+        }
+        return post_request_session(config.pt_host+url, params)
+
+    @staticmethod
+    def bet():
+        try:
+            # 获取摩天轮信息
+            greed_round_id = 0
+            for i in range(10):
+                greed_index = Greedy.greedy_index(config.pt_payUid)
+                greed_state = greed_index['body']['data']['state']
+                if greed_state == '1':
+                    greed_round_id = greed_index['body']['data']['round_id']
+                    break
+                else:
+                    time.sleep(5)  # 等待摩天轮变为可投注状态
+                    continue
+
+            # 下注
+            bet_money = 0
+            for i in range(6):
+                greed_vid = random.randint(1, 8)
+                greed_counter = random.choice([10, 50, 100, 1000])
+                bet_money = bet_money + greed_counter
+                Greedy.greedy_stake(config.pt_payUid, greed_vid, greed_counter, greed_round_id)
+                print(Greedy)
+
+            # 获取开奖数据
+            greed_counter_all = 0
+            greedy_prize = 0
+            for i in range(10):
+                greedy_player_data = conMysql.select_greedy_prize(config.pt_payUid, greed_round_id)
+                if greedy_player_data == 0:
+                    time.sleep(5)  # 等待摩天轮开奖
+                    continue
+                else:
+                    greed_counter_all = greedy_player_data[0]
+                    greedy_prize = greedy_player_data[1]
+            if bet_money == greed_counter_all > 0:
+                return [greed_counter_all, greedy_prize]
+            else:
+                print('下注失败')
+        except Exception as e:
+            print(e)
+
