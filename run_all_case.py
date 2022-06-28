8b6f920a3967440f9855dc76d2ac4792f1efce1f
liximing1013 <973878442@qq.com>
2022-06-28 21:19:45 +0800

    ‘提交修改’

--- run_all_case.py
+++ run_all_case.py
@@ -43,7 +43,7 @@ def main(appInfo):
         else:
             Logs.get_log('runCode.log').info('NoRun')
     elif appInfo == config.appName['Partying']:
-        checkPath('/hme')
+        checkPath('/home/webroot/release_oversea/oversea-serve')
         if updateCode.autoGitPull(appInfo):
             updateTime('write', now=str(int(time.time())))
             test_result = unittest.TextTestRunner(verbosity=3).run(all_case(appInfo))
