8f462ac43fd42526a0b9284aec000e569d5e6ccc
liximing1013 <973878442@qq.com>
2022-09-07 16:44:56 +0800

    ‘提交修改’

--- others/bot_tea.py
+++ others/bot_tea.py
@@ -1,7 +1,9 @@
 import requests
 import datetime
 import time
+import random
 from chinese_calendar import is_holiday
+
 
 def getHoliday():
     now_year = int(time.strftime('%Y', time.localtime(time.time())))
@@ -12,11 +14,12 @@ def getHoliday():
     print(is_holiday(holiday))
     return is_holiday(holiday)
 
+
 def robot():
     url = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=e317861a-d1ec-4ac4-af96-9d4b8f12d9d6'
     # url= 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=ddf98ffd-fd35-42be-8362-5e485b08226a'  # 测试用
     headers = {'Content-Type': 'application/json'}
-    reason = {'【曼哈顿博士】': '恭喜您成为尊贵的五菱宏光车主，愿红鹰展翅，照亮你加班的路程，愿更大的后备箱，能够帮你拉更多的货'}
+    reason = {'【曼哈顿博士】': '{}'.format(random.choice(reason_list))}
     now = time.strftime('%H:%M', time.localtime(time.time()))
     title = "下午茶时间到:当前时间-{}, 放松一下".format(now)
     des = '跪求大哥赞助'
@@ -54,6 +57,7 @@ def robot():
         }
         requests.post(url, headers=headers, json=data)
 
+
 def dictToList(result_dict):
     list_case = []
     for k, v in result_dict.items():
@@ -62,6 +66,15 @@ def dictToList(result_dict):
     case = '\n'.join(list_case)
     return case
 
+
+reason_list = ['恭喜您成为尊贵的梅赛德斯奔驰车主，愿三叉星辉照亮您的事业与前程。',
+               '恭喜您成为尊贵的保时捷车主，愿黑马金盾守护您高贵的人生。',
+               '恭喜您成为尊贵的奥迪车主，愿四环星辉照亮您的事业前程，愿世间美好从此与你环环相扣。',
+               '恭喜您成为尊贵的宝马车主，愿浩瀚的蓝天白云，承载您的美好征程。',
+               '恭喜您成为尊贵的红旗车主，愿红旗与您一起乘风破浪，蒸蒸日上，目光所致，皆是华夏，五星闪耀，皆是信仰。',
+               '恭喜您成为尊贵的大众车主，愿VW星辉照耀您未来的事业前程，愿您以后踏遍大好河山，众拥天下。',
+               '恭喜您成为尊贵的凯迪拉克车主，愿七彩圣盾保佑您的事业前程，所有的伟大都源于一次勇敢的开始，愿世间每一次泡澡都能拥有新的感悟。',
+               '恭喜您成为尊贵的五菱宏光车主，愿红鹰展翅，照亮你加班的路程，愿更大的后备箱，能够帮你拉更多的货。']
 
 if __name__ == '__main__':
     robot()
