224864ac533261668c6442bbf597fdb84c4ea31b
liximing1013 <973878442@qq.com>
2022-10-09 18:50:02 +0800

    ‘提交修改&代码优化’

--- others/bot_dinner.py
+++ others/bot_dinner.py
@@ -33,6 +33,19 @@ def robot():
             ]
         }
     }
+    data = {
+               "fallback": "Required text summary of the attachment that is shown by clients that understand attachments but choose not to show them.",
+               "text": "Optional text that should appear within the attachment",
+               "pretext": "Optional text that should appear above the formatted data",
+               "color": "#36a64f",
+        "fields": [
+            {
+                "title": "Required Field Title",
+                "value": "Text value of the field. May contain standard message markup and must be escaped as normal. May be multi-line.",
+                "short": 'false'
+            }
+        ]
+    }
     if getHoliday():
         return False
     r = requests.post(
