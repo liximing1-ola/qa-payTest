04b6c30849f70cb4ff7e090ff858b7f76291df30
liximing1013 <973878442@qq.com>
2022-10-21 15:58:17 +0800

    ‘提交修改&代码优化’

--- common/method.py
+++ common/method.py
@@ -16,7 +16,7 @@ def dictToList(result_dict):
     list_case = []
     for k, v in result_dict.items():
         # list_case.append('<font color="comment">{}-</font>,<font color=\"info\">{}</font>'.format(v, k))
-        list_case.append('scene:{}-{}'.format(k, v))
+        list_case.append('scene-{}:{}'.format(k, v))
     case = '\n'.join(list_case)
     # with open(path + 'result.md', 'a', encoding='utf-8') as r:
     #    r.writelines(case)
