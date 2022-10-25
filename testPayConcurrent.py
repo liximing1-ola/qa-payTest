2ada6d73057aaa55de4e0610dddddfff7ad3ba07
liximing1013 <973878442@qq.com>
2022-10-25 11:16:56 +0800

    ‘提交修改&代码优化’

--- testPayConcurrent.py
+++ testPayConcurrent.py
@@ -17,8 +17,7 @@ class TestPayConcurrent:
     # 物品使用接口
     commodity_use = config.bb_host + 'commodity/use?package=com.imbb.banban.android'
 
-    @staticmethod
-    def startPayCreateReady():
+    def startPayCreateReady(self):
         """
         用例描述：
         构造背包内购买礼物场景
@@ -34,13 +33,12 @@ class TestPayConcurrent:
         mysql.updateMoneySql(config.payUid, 10000)
         mysql.deleteUserCommoditySql(config.payUid)
         data = basicData.encodeData(payType='shop-buy', cid=cid, money=9900, num=1)
-        res = post_request_session(url=TestPayConcurrent.pay_url, data=data)
+        res = post_request_session(url=self.pay_url, data=data)
         assert_code(res['code'], 200)
         assert_equal(mysql.selectAllMoneySql(config.payUid), 100)
         assert_equal(mysql.checkUserAllCommoditySql(config.payUid), 1)
 
-    @staticmethod
-    def payCreateConcurrent():
+    def payCreateConcurrent(self):
         """
         用例描述：
         验证商城购买的道具在房间内赠送给其他人
@@ -56,7 +54,7 @@ class TestPayConcurrent:
         mysql.updateMoneySql(config.rewardUid)
         cid = int(mysql.getUserCommodityIdSql(bag_gift_cid, config.payUid))
         payload = 'platform=available&type=package&money=9900&params=%7B%22rid%22%3A193185484%2C%22uids%22%3A%22105002312%22%2C%22positions%22%3A%220%22%2C%22position%22%3A-1%2C%22giftId%22%3A54%2C%22giftNum%22%3A1%2C%22price%22%3A9900%2C%22cid%22%3A{}%2C%22ctype%22%3A%22gift%22%2C%22duction_money%22%3A0%2C%22version%22%3A2%2C%22num%22%3A1%2C%22gift_type%22%3A%22normal%22%2C%22star%22%3A0%2C%22refer%22%3A%22%E7%83%AD%E9%97%A8%3Aroom%22%2C%22useCoin%22%3A-1%7D'.format(cid)
-        res = post_request_session(url=TestPayConcurrent.pay_url, data=payload)
+        res = post_request_session(url=self.pay_url, data=payload)
         assert_code(res['code'], 200)
         getValue(res)
 
@@ -68,13 +66,12 @@ class TestPayConcurrent:
         assert_equal(Consts.success_num, 1)
         Consts.fail_num=0
 
-    @staticmethod
-    def test_01_payCreate(num_times):
+    def test_01_payCreate(self, num_times):
         d = '并发打赏背包礼物的场景'
-        TestPayConcurrent.startPayCreateReady()
+        self.startPayCreateReady()
         threads = []
         for i in range(num_times):
-            thread = gevent.spawn(TestPayConcurrent.payCreateConcurrent)
+            thread = gevent.spawn(self.payCreateConcurrent)
             threads.append(thread)
         gevent.joinall(threads)
         TestPayConcurrent.endPayCreate()
@@ -165,11 +162,10 @@ class TestPayConcurrent:
         TestPayConcurrent.endCommodityPresent()
         Consts.case_list_c[d] = Consts.result
 
-    @staticmethod
-    def main(num):
-        TestPayConcurrent.test_01_payCreate(num)
-        TestPayConcurrent.test_02_commodityUse(num)
-        TestPayConcurrent.test_03_commodityPresent(num)
+    def main(self, num):
+        self.test_01_payCreate(num)
+        # self.test_02_commodityUse(num)
+        # self.test_03_commodityPresent(num)
         case_list = method.dictToListSlack(Consts.case_list_c)
         des = "{}\n".format(case_list)
         Logs.get_log('concurrentCaseResult.log').info(des)
@@ -177,4 +173,5 @@ class TestPayConcurrent:
 
 
 if __name__=='__main__':
-    TestPayConcurrent.main(10)
+    p = TestPayConcurrent()
+    p.main(10)
