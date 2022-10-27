c4d5a29975b7a4807b919c970aa8ae7c721c9fb7
liximing1013 <973878442@qq.com>
2022-10-27 18:43:00 +0800

    ‘提交修改&代码优化’

--- common/Session.py
+++ common/Session.py
@@ -51,12 +51,12 @@ class Session:
                 params = Yaml.read_yaml('Basic.yml', 'params_teammate_qq')
                 login_url = config.bb_qqLogin_url + '?' + params + '&package=com.im.duck.android'  # 7.22修改，请求接口加包名限制
                 body = Yaml.read_yaml('Basic.yml', 'data_teammate_qq')
-                session = requests.session()
                 print(1)
                 print(headers)
                 print(params)
                 print(login_url)
                 print(body)
+                session = requests.session()
                 res = session.post(login_url, data=body, headers=headers, verify=False)
                 res.raise_for_status()
                 res = res.json()
