c3b0dd3c8bab93984b20e02e9d886e65d197cf8a
liximing1013 <973878442@qq.com>
2022-11-01 18:40:06 +0800

    ‘提交修改&代码优化’

--- case/test_pay_superBroker.py
+++ case/test_pay_superBroker.py
@@ -10,7 +10,7 @@ from common.runFailed import Retry
 from common.Consts import case_list_b, result
 from common.conRedis import conRedis
 @Retry
-# @unittest.skip('2022/11/1 网赚分成下线')
+@unittest.skip('2022/11/1 网赚分成下线')
 class TestPayCreate(unittest.TestCase):
 
     @classmethod
@@ -226,7 +226,6 @@ class TestPayCreate(unittest.TestCase):
         assert_equal(conMysql.selectUserInfoSql('sum_money', config.payUid), 400)
         case_list_b[des] = result
 
-    @unittest.skip
     @pytest.mark.run(order=8)
     def test_08_NormalRoomPayArtist_620(self, des='普通房指定工会有经纪人(1j)只艺人收到62%'):
         """
@@ -258,7 +257,6 @@ class TestPayCreate(unittest.TestCase):
         assert_equal(conMysql.selectUserInfoSql('sum_money', config.payUid), 0)
         case_list_b[des] = result
 
-    @unittest.skip
     @pytest.mark.run(order=9)
     def test_09_starRoomWhiteUserPay_70(self, des='网赚房无公会无经纪人白名单艺人收70%个人魅力值'):
         """
