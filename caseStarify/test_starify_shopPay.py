7db037dd57a2fc1b010dd7680c147d93d92435c8
wzx <Wu.Zhenxing@olaola.chat>
2022-11-09 15:56:46 +0800

    新增 商城 case

--- caseStarify/test_starify_shopPay.py
+++ caseStarify/test_starify_shopPay.py
@@ -16,13 +16,13 @@ class TestPayCreate(unittest.TestCase):
     def test_shop_001(self, des='星币充足,商城购买-头像框'):
         commodity = commodity_config['header']
         #  sql:打赏者starify_payUid 修改余额=10000
-        conMysql.updateMoneySql(starify_payUid, 10000)
+        conMysql.updateMoneySql(starify_payUid, 100000)
         data = deal_pay_data("shop_buy", commodity['cid'])
         res = post_request_session_starify(config.starify_pay_url, data, tokenName='starify')
         assert_code(res['code'])
         assert_body(res['body'], 'success', True, reason_starify(des, res))
         #  sql:starify_payUid 查询余额=0
-        assert_equal(conMysql.selectUserInfoSql('star_coin', starify_payUid), 10000-3*commodity['price'])
+        assert_equal(conMysql.selectUserInfoSql('star_coin', starify_payUid), 100000-3*commodity['price'])
         case_list[des] = result
     # def test_shop_001(self, des='星币充足,商城购买-进场横幅'):
     #     pass
@@ -33,24 +33,3 @@ class TestPayCreate(unittest.TestCase):
     # def test_shop_001(self, des='星币充足,商城购买单个道具场景'):
     #     pass
 
-
-if __name__ == '__main__':
-    """
-    http://47.243.83.154/go/starify/pay/create?package=com.starify.ola.android&_ipv=1&_platform=android&_index=175&_model=GM1910&_timestamp=1667976298&_abi=arm64-v8a&format=pb&_sign=f99ad125f640fd14985a3002d1906fcd&_blid=124498&_versionName=1.0.5.2&_versionCode=100005002
-    """
-    params = {
-        "_abi": "arm64-v8a",
-        "format": "pb",
-        "_sign": "f99ad125f640fd14985a3002d1906fcd",
-        "_blid": "124498",
-        "_versionName": "1.0.5.2",
-        "_versionCode": "100005002",
-    }
-    data={
-        """
-op_type	shop_buy
-params	{"cid":83,"sale_level":1,"count":1}
-money_type	star_coin
-total_money	893584
-        """
-    }
