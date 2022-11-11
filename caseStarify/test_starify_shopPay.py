8ff987944c54def553ebfd1af1d22535b92c331f
wzx <Wu.Zhenxing@olaola.chat>
2022-11-11 10:40:16 +0800

    修改 参数

--- caseStarify/test_starify_shopPay.py
+++ caseStarify/test_starify_shopPay.py
@@ -1,111 +1,111 @@
-# import math
-# import unittest
-#
-# from caseStarify.deal_data import deal_pay_data
-# from caseStarify.need_data import *
-# from caseStarify.tools import deal_num
-# from common.Assert import *
-# from common.Consts import case_list, result
-# # from common.method import reason
-# from common.Request import post_request_session_starify
-# from common.conStarifyMysql import conMysql
-# from common.method import reason_starify
-# from common.runFailed import Retry
-#
-#
-# @Retry(max_n=1)
-# class TestPayCreate(unittest.TestCase):
-#     def test_shop_001(self, des='星币充足,商城购买-头像框,3天'):
-#         commodity = commodity_config['header']
-#         sale_level = 1
-#         #  sql:打赏者starify_payUid 修改余额=10000
-#         conMysql.updateMoneySql(starify_payUid, 100000)
-#         #  sql:打赏者starify_payUid 清空背包礼物
-#         conMysql.deleteUserAccountSql('user_commodity', starify_payUid)
-#         data = deal_pay_data("shop_buy", commodity, sale_level=sale_level)
-#         res = post_request_session_starify(config.starify_pay_url, data, tokenName='starify')
-#         assert_code(res['code'])
-#         assert_body(res['body'], 'success', True, reason_starify(des, res))
-#         #  sql:starify_payUid 查询余额=0
-#         cost = deal_num(commodity[f'level_{sale_level}']['day'] * commodity[f'level_{sale_level}']['rate'] * commodity[
-#             'price'])
-#         assert_equal(conMysql.selectUserInfoSql('star_coin', starify_payUid), 100000 - cost)
-#         #  sql:检查背包物品=1
-#         assert_equal(conMysql.selectUserInfoSql('commodity_num', starify_payUid, commodity['cid'], 86400 * 3), 1)
-#         case_list[des] = result
-#
-#     def test_shop_002(self, des='星币充足,商城购买-头像框,7天'):
-#         commodity = commodity_config['header']
-#         sale_level = 2
-#         #  sql:打赏者starify_payUid 修改余额=10000
-#         conMysql.updateMoneySql(starify_payUid, 100000)
-#         #  sql:打赏者starify_payUid 清空背包礼物
-#         conMysql.deleteUserAccountSql('user_commodity', starify_payUid)
-#         data = deal_pay_data("shop_buy", commodity, sale_level=sale_level)
-#         res = post_request_session_starify(config.starify_pay_url, data, tokenName='starify')
-#         assert_code(res['code'])
-#         assert_body(res['body'], 'success', True, reason_starify(des, res))
-#         #  sql:starify_payUid 查询余额=0
-#         cost = deal_num(commodity[f'level_{sale_level}']['day'] * commodity[f'level_{sale_level}']['rate'] * commodity[
-#             'price'])
-#         assert_equal(conMysql.selectUserInfoSql('star_coin', starify_payUid), 100000 - cost)
-#         #  sql:检查背包物品=1
-#         assert_equal(conMysql.selectUserInfoSql('commodity_num', starify_payUid, commodity['cid'], 86400 * 7), 1)
-#         case_list[des] = result
-#
-#     def test_shop_003(self, des='星币充足,商城购买-头像框,15天'):
-#         commodity = commodity_config['header']
-#         sale_level = 3
-#         #  sql:打赏者starify_payUid 修改余额=10000
-#         conMysql.updateMoneySql(starify_payUid, 100000)
-#         #  sql:打赏者starify_payUid 清空背包礼物
-#         conMysql.deleteUserAccountSql('user_commodity', starify_payUid)
-#         data = deal_pay_data("shop_buy", commodity, sale_level=sale_level)
-#         res = post_request_session_starify(config.starify_pay_url, data, tokenName='starify')
-#         assert_code(res['code'])
-#         assert_body(res['body'], 'success', True, reason_starify(des, res))
-#         #  sql:starify_payUid 查询余额=0
-#         cost = deal_num(commodity[f'level_{sale_level}']['day'] * commodity[f'level_{sale_level}']['rate'] * commodity[
-#             'price'])
-#         assert_equal(conMysql.selectUserInfoSql('star_coin', starify_payUid), 100000 - cost)
-#         #  sql:检查背包物品=1
-#         assert_equal(conMysql.selectUserInfoSql('commodity_num', starify_payUid, commodity['cid'], 86400 * 15), 1)
-#         case_list[des] = result
-#
-#     def test_shop_004(self, des='星币充足,商城购买-进场横幅,3天'):
-#         commodity = commodity_config['effect']
-#         sale_level = 1
-#         #  sql:打赏者starify_payUid 修改余额=10000
-#         conMysql.updateMoneySql(starify_payUid, 100000)
-#         #  sql:打赏者starify_payUid 清空背包礼物
-#         conMysql.deleteUserAccountSql('user_commodity', starify_payUid)
-#         data = deal_pay_data("shop_buy", commodity, sale_level=sale_level)
-#         res = post_request_session_starify(config.starify_pay_url, data, tokenName='starify')
-#         assert_code(res['code'])
-#         assert_body(res['body'], 'success', True, reason_starify(des, res))
-#         #  sql:starify_payUid 查询余额=0
-#         cost = deal_num(commodity[f'level_{sale_level}']['day'] * commodity[f'level_{sale_level}']['rate'] * commodity[
-#             'price'])
-#         assert_equal(conMysql.selectUserInfoSql('star_coin', starify_payUid), 100000 - cost)
-#         #  sql:检查背包物品=1
-#         assert_equal(conMysql.selectUserInfoSql('commodity_num', starify_payUid, commodity['cid'], 86400 * 3), 1)
-#         case_list[des] = result
-#
-#     def test_shop_005(self, des='星币充足,商城购买-麦上光圈,3天'):
-#         commodity = commodity_config['ring']
-#         sale_level = 1
-#         #  sql:打赏者starify_payUid 修改余额=10000
-#         conMysql.updateMoneySql(starify_payUid, 100000)
-#         #  sql:打赏者starify_payUid 清空背包礼物
-#         conMysql.deleteUserAccountSql('user_commodity', starify_payUid)
-#         data = deal_pay_data("shop_buy", commodity, sale_level=sale_level)
-#         res = post_request_session_starify(config.starify_pay_url, data, tokenName='starify')
-#         assert_code(res['code'])
-#         assert_body(res['body'], 'success', True, reason_starify(des, res))
-#         #  sql:starify_payUid 查询余额=0
-#         cost = deal_num(commodity[f'level_{sale_level}']['day'] * commodity[f'level_{sale_level}']['rate'] * commodity[
-#             'price'])
-#         assert_equal(conMysql.selectUserInfoSql('star_coin', starify_payUid), 100000 - cost)
-#         #  sql:检查背包物品=1
-#         assert_equal(conMysql.selectUserInfoSql('commodity_num', starify_payUid, commodity['cid'], 86400 * 3), 1)
-#         case_list[des] = result
+import math
+import unittest
+
+from caseStarify.deal_data import deal_pay_data
+from caseStarify.need_data import *
+from caseStarify.tools import deal_num
+from common.Assert import *
+from common.Consts import case_list, result
+# from common.method import reason
+from common.Request import post_request_session_starify
+from common.conStarifyMysql import conMysql
+from common.method import reason_starify
+from common.runFailed import Retry
+
+
+@Retry(max_n=1)
+class TestPayCreate(unittest.TestCase):
+    def test_shop_001(self, des='星币充足,商城购买-头像框,3天'):
+        commodity = commodity_config['header']
+        sale_level = 1
+        #  sql:打赏者starify_payUid 修改余额=10000
+        conMysql.updateMoneySql(starify_payUid, 100000)
+        #  sql:打赏者starify_payUid 清空背包礼物
+        conMysql.deleteUserAccountSql('user_commodity', starify_payUid)
+        data = deal_pay_data("shop_buy", commodity, sale_level=sale_level)
+        res = post_request_session_starify(config.starify_pay_url, data, tokenName='starify')
+        assert_code(res['code'])
+        assert_body(res['body'], 'success', True, reason_starify(des, res))
+        #  sql:starify_payUid 查询余额=0
+        cost = deal_num(commodity[f'level_{sale_level}']['day'] * commodity[f'level_{sale_level}']['rate'] * commodity[
+            'price'])
+        assert_equal(conMysql.selectUserInfoSql('star_coin', starify_payUid), 100000 - cost)
+        #  sql:检查背包物品=1
+        assert_equal(conMysql.selectUserInfoSql('commodity_num', starify_payUid, commodity['cid'], 86400 * 3), 1)
+        case_list[des] = result
+
+    def test_shop_002(self, des='星币充足,商城购买-头像框,7天'):
+        commodity = commodity_config['header']
+        sale_level = 2
+        #  sql:打赏者starify_payUid 修改余额=10000
+        conMysql.updateMoneySql(starify_payUid, 100000)
+        #  sql:打赏者starify_payUid 清空背包礼物
+        conMysql.deleteUserAccountSql('user_commodity', starify_payUid)
+        data = deal_pay_data("shop_buy", commodity, sale_level=sale_level)
+        res = post_request_session_starify(config.starify_pay_url, data, tokenName='starify')
+        assert_code(res['code'])
+        assert_body(res['body'], 'success', True, reason_starify(des, res))
+        #  sql:starify_payUid 查询余额=0
+        cost = deal_num(commodity[f'level_{sale_level}']['day'] * commodity[f'level_{sale_level}']['rate'] * commodity[
+            'price'])
+        assert_equal(conMysql.selectUserInfoSql('star_coin', starify_payUid), 100000 - cost)
+        #  sql:检查背包物品=1
+        assert_equal(conMysql.selectUserInfoSql('commodity_num', starify_payUid, commodity['cid'], 86400 * 7), 1)
+        case_list[des] = result
+
+    def test_shop_003(self, des='星币充足,商城购买-头像框,15天'):
+        commodity = commodity_config['header']
+        sale_level = 3
+        #  sql:打赏者starify_payUid 修改余额=10000
+        conMysql.updateMoneySql(starify_payUid, 100000)
+        #  sql:打赏者starify_payUid 清空背包礼物
+        conMysql.deleteUserAccountSql('user_commodity', starify_payUid)
+        data = deal_pay_data("shop_buy", commodity, sale_level=sale_level)
+        res = post_request_session_starify(config.starify_pay_url, data, tokenName='starify')
+        assert_code(res['code'])
+        assert_body(res['body'], 'success', True, reason_starify(des, res))
+        #  sql:starify_payUid 查询余额=0
+        cost = deal_num(commodity[f'level_{sale_level}']['day'] * commodity[f'level_{sale_level}']['rate'] * commodity[
+            'price'])
+        assert_equal(conMysql.selectUserInfoSql('star_coin', starify_payUid), 100000 - cost)
+        #  sql:检查背包物品=1
+        assert_equal(conMysql.selectUserInfoSql('commodity_num', starify_payUid, commodity['cid'], 86400 * 15), 1)
+        case_list[des] = result
+
+    def test_shop_004(self, des='星币充足,商城购买-进场横幅,3天'):
+        commodity = commodity_config['effect']
+        sale_level = 1
+        #  sql:打赏者starify_payUid 修改余额=10000
+        conMysql.updateMoneySql(starify_payUid, 100000)
+        #  sql:打赏者starify_payUid 清空背包礼物
+        conMysql.deleteUserAccountSql('user_commodity', starify_payUid)
+        data = deal_pay_data("shop_buy", commodity, sale_level=sale_level)
+        res = post_request_session_starify(config.starify_pay_url, data, tokenName='starify')
+        assert_code(res['code'])
+        assert_body(res['body'], 'success', True, reason_starify(des, res))
+        #  sql:starify_payUid 查询余额=0
+        cost = deal_num(commodity[f'level_{sale_level}']['day'] * commodity[f'level_{sale_level}']['rate'] * commodity[
+            'price'])
+        assert_equal(conMysql.selectUserInfoSql('star_coin', starify_payUid), 100000 - cost)
+        #  sql:检查背包物品=1
+        assert_equal(conMysql.selectUserInfoSql('commodity_num', starify_payUid, commodity['cid'], 86400 * 3), 1)
+        case_list[des] = result
+
+    def test_shop_005(self, des='星币充足,商城购买-麦上光圈,3天'):
+        commodity = commodity_config['ring']
+        sale_level = 1
+        #  sql:打赏者starify_payUid 修改余额=10000
+        conMysql.updateMoneySql(starify_payUid, 100000)
+        #  sql:打赏者starify_payUid 清空背包礼物
+        conMysql.deleteUserAccountSql('user_commodity', starify_payUid)
+        data = deal_pay_data("shop_buy", commodity, sale_level=sale_level)
+        res = post_request_session_starify(config.starify_pay_url, data, tokenName='starify')
+        assert_code(res['code'])
+        assert_body(res['body'], 'success', True, reason_starify(des, res))
+        #  sql:starify_payUid 查询余额=0
+        cost = deal_num(commodity[f'level_{sale_level}']['day'] * commodity[f'level_{sale_level}']['rate'] * commodity[
+            'price'])
+        assert_equal(conMysql.selectUserInfoSql('star_coin', starify_payUid), 100000 - cost)
+        #  sql:检查背包物品=1
+        assert_equal(conMysql.selectUserInfoSql('commodity_num', starify_payUid, commodity['cid'], 86400 * 3), 1)
+        case_list[des] = result
--- caseStarify/test_starify_workGift.py
+++ caseStarify/test_starify_workGift.py
@@ -1,82 +1,82 @@
-# import unittest
-#
-# from caseStarify.deal_data import deal_pay_data
-# from caseStarify.need_data import *
-# from common.Assert import *
-# from common.Consts import case_list, result
-# # from common.method import reason
-# from common.Request import post_request_session_starify
-# from common.conStarifyMysql import conMysql
-# from common.method import reason_starify
-# from common.runFailed import Retry
-#
-#
-# @Retry(max_n=1)
-# class TestPayCreate(unittest.TestCase):
-#     def test_work_001(self, des='星币余额充足,作品打赏,礼物类型=安可'):
-#         commodity = commodity_config['2']
-#         #  sql:清除作品已被打赏的标记
-#         conMysql.deleteUserAccountSql("user_work_reward", starify_payUid, starify_work_state['todo'])
-#         #  sql:打赏者starify_payUid 修改余额=2
-#         conMysql.updateMoneySql(starify_payUid, 2)
-#         data = deal_pay_data("work", commodity, work_state="todo")
-#         res = post_request_session_starify(config.starify_pay_url, data, tokenName='starify')
-#         assert_code(res['code'])
-#         assert_body(res['body'], 'success', True, reason_starify(des, res))
-#         #  sql:starify_payUid 查询余额=0
-#         assert_equal(conMysql.selectUserInfoSql('star_coin', starify_payUid), 0)
-#         case_list[des] = result
-#
-#     def test_work_002(self, des='作品打赏,星币余额=0'):
-#         commodity = commodity_config['2']
-#         #  sql:清除作品已被打赏的标记
-#         conMysql.deleteUserAccountSql("user_work_reward", starify_payUid, starify_work_state['todo'])
-#         #  sql:starify_payUid 修改余额=0
-#         conMysql.updateMoneySql(starify_payUid, 0)
-#         data = deal_pay_data("work", commodity, work_state="todo")
-#         res = post_request_session_starify(config.starify_pay_url, data, tokenName='starify')
-#         assert_code(res['code'])
-#         assert_body(res['body'], 'msg', '余额不足', reason_starify(des, res))
-#         #  sql:starify_payUid 查询余额=0
-#         assert_equal(conMysql.selectUserInfoSql('star_coin', starify_payUid), 0)
-#         case_list[des] = result
-#
-#     def test_work_003(self, des='作品打赏,星币余额<礼物价值'):
-#         commodity = commodity_config['2']
-#         #  sql:清除作品已被打赏的标记
-#         conMysql.deleteUserAccountSql("user_work_reward", starify_payUid, starify_work_state['todo'])
-#         #  sql:starify_payUid 修改余额=1
-#         conMysql.updateMoneySql(starify_payUid, 1)
-#         data = deal_pay_data("work", commodity, work_state="todo")
-#         res = post_request_session_starify(config.starify_pay_url, data, tokenName='starify')
-#         assert_code(res['code'])
-#         assert_body(res['body'], 'msg', "余额不足", reason_starify(des, res))
-#         #  sql:starify_payUid 查询余额=1
-#         assert_equal(conMysql.selectUserInfoSql('star_coin', starify_payUid), 1)
-#         case_list[des] = result
-#
-#     def test_work_004(self, des='作品打赏,重复打赏'):
-#         commodity = commodity_config['2']
-#         #  sql:starify_payUid 修改余额=2
-#         conMysql.updateMoneySql(starify_payUid, 2)
-#         data = deal_pay_data("work", commodity, work_state="done")
-#         res = post_request_session_starify(config.starify_pay_url, data, tokenName='starify')
-#         assert_code(res['code'])
-#         assert_body(res['body'], 'msg', '同一个星币礼物只能打赏同一个作品一次', reason_starify(des, res))
-#         #  sql:starify_payUid 查询余额=2
-#         assert_equal(conMysql.selectUserInfoSql('star_coin', starify_payUid), 2)
-#         case_list[des] = result
-#
-#     def test_work_005(self, des='作品打赏,星币余额充足,礼物类型=星币'):
-#         commodity = commodity_config['1']
-#         #  sql:清除作品已被打赏的标记
-#         conMysql.deleteUserAccountSql("user_work_reward", starify_payUid, starify_work_state['todo'])
-#         #  sql:starify_payUid 修改余额
-#         conMysql.updateMoneySql(starify_payUid, 1)
-#         data = deal_pay_data("work", commodity, "todo")
-#         res = post_request_session_starify(config.starify_pay_url, data, tokenName='starify')
-#         assert_code(res['code'])
-#         assert_body(res['body'], 'success', True, reason_starify(des, res))
-#         #  sql:starify_payUid 查询余额
-#         assert_equal(conMysql.selectUserInfoSql('star_coin', starify_payUid), 0)
-#         case_list[des] = result
+import unittest
+
+from caseStarify.deal_data import deal_pay_data
+from caseStarify.need_data import *
+from common.Assert import *
+from common.Consts import case_list, result
+# from common.method import reason
+from common.Request import post_request_session_starify
+from common.conStarifyMysql import conMysql
+from common.method import reason_starify
+from common.runFailed import Retry
+
+
+@Retry(max_n=1)
+class TestPayCreate(unittest.TestCase):
+    def test_work_001(self, des='星币余额充足,作品打赏,礼物类型=安可'):
+        commodity = commodity_config['2']
+        #  sql:清除作品已被打赏的标记
+        conMysql.deleteUserAccountSql("user_work_reward", starify_payUid, starify_work_state['todo'])
+        #  sql:打赏者starify_payUid 修改余额=2
+        conMysql.updateMoneySql(starify_payUid, 2)
+        data = deal_pay_data("work", commodity, work_state="todo")
+        res = post_request_session_starify(config.starify_pay_url, data, tokenName='starify')
+        assert_code(res['code'])
+        assert_body(res['body'], 'success', True, reason_starify(des, res))
+        #  sql:starify_payUid 查询余额=0
+        assert_equal(conMysql.selectUserInfoSql('star_coin', starify_payUid), 0)
+        case_list[des] = result
+
+    def test_work_002(self, des='作品打赏,星币余额=0'):
+        commodity = commodity_config['2']
+        #  sql:清除作品已被打赏的标记
+        conMysql.deleteUserAccountSql("user_work_reward", starify_payUid, starify_work_state['todo'])
+        #  sql:starify_payUid 修改余额=0
+        conMysql.updateMoneySql(starify_payUid, 0)
+        data = deal_pay_data("work", commodity, work_state="todo")
+        res = post_request_session_starify(config.starify_pay_url, data, tokenName='starify')
+        assert_code(res['code'])
+        assert_body(res['body'], 'msg', '余额不足', reason_starify(des, res))
+        #  sql:starify_payUid 查询余额=0
+        assert_equal(conMysql.selectUserInfoSql('star_coin', starify_payUid), 0)
+        case_list[des] = result
+
+    def test_work_003(self, des='作品打赏,星币余额<礼物价值'):
+        commodity = commodity_config['2']
+        #  sql:清除作品已被打赏的标记
+        conMysql.deleteUserAccountSql("user_work_reward", starify_payUid, starify_work_state['todo'])
+        #  sql:starify_payUid 修改余额=1
+        conMysql.updateMoneySql(starify_payUid, 1)
+        data = deal_pay_data("work", commodity, work_state="todo")
+        res = post_request_session_starify(config.starify_pay_url, data, tokenName='starify')
+        assert_code(res['code'])
+        assert_body(res['body'], 'msg', "余额不足", reason_starify(des, res))
+        #  sql:starify_payUid 查询余额=1
+        assert_equal(conMysql.selectUserInfoSql('star_coin', starify_payUid), 1)
+        case_list[des] = result
+
+    def test_work_004(self, des='作品打赏,重复打赏'):
+        commodity = commodity_config['2']
+        #  sql:starify_payUid 修改余额=2
+        conMysql.updateMoneySql(starify_payUid, 2)
+        data = deal_pay_data("work", commodity, work_state="done")
+        res = post_request_session_starify(config.starify_pay_url, data, tokenName='starify')
+        assert_code(res['code'])
+        assert_body(res['body'], 'msg', '同一个星币礼物只能打赏同一个作品一次', reason_starify(des, res))
+        #  sql:starify_payUid 查询余额=2
+        assert_equal(conMysql.selectUserInfoSql('star_coin', starify_payUid), 2)
+        case_list[des] = result
+
+    def test_work_005(self, des='作品打赏,星币余额充足,礼物类型=星币'):
+        commodity = commodity_config['1']
+        #  sql:清除作品已被打赏的标记
+        conMysql.deleteUserAccountSql("user_work_reward", starify_payUid, starify_work_state['todo'])
+        #  sql:starify_payUid 修改余额
+        conMysql.updateMoneySql(starify_payUid, 1)
+        data = deal_pay_data("work", commodity, "todo")
+        res = post_request_session_starify(config.starify_pay_url, data, tokenName='starify')
+        assert_code(res['code'])
+        assert_body(res['body'], 'success', True, reason_starify(des, res))
+        #  sql:starify_payUid 查询余额
+        assert_equal(conMysql.selectUserInfoSql('star_coin', starify_payUid), 0)
+        case_list[des] = result
