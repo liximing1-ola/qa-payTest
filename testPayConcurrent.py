057830c1227fd208ed9347996b280edf5e55c845
li.ximing <li.ximing@olaola.chat>
2023-02-15 18:04:27 +0800

    '分成体系优化'

--- testPayConcurrent.py
+++ testPayConcurrent.py
@@ -36,13 +36,13 @@ class TestPayConcurrent:
         4.检查购买者余额 (10000-9900=100)
         5.检查背包内物品
         """
-        mysql.updateMoneySql(config.payUid, 20000)
+        mysql.updateMoneySql(config.payUid, 10000)
         mysql.deleteUserCommoditySql(config.payUid)
-        data = encodeData(payType='shop-buy', cid=self.commodity_id['cid_340'], money=9900, num=2)
+        data = encodeData(payType='shop-buy', cid=self.commodity_id['cid_340'], money=9900, num=1)
         res = post_request_session(url=self.php_urL['pay_url'], data=data)
         assert_code(res['code'], 200)
-        assert_equal(mysql.selectAllMoneySql(config.payUid), 200)
-        assert_equal(mysql.checkUserAllCommoditySql(config.payUid), 2)
+        assert_equal(mysql.selectAllMoneySql(config.payUid), 100)
+        assert_equal(mysql.checkUserAllCommoditySql(config.payUid), 1)
 
     def payCreateConcurrent(self):
         """
@@ -55,22 +55,19 @@ class TestPayConcurrent:
         4.检查背包内物品
         5.检查被打赏者余额 990*0.62 = 6138
         """
+        mysql.updateMoneySql(config.payUid)
+        mysql.updateMoneySql(config.rewardUid)
         cid = int(mysql.getUserCommodityIdSql(self.commodity_id['cid_340'], config.payUid))
-        data = encodeData(payType='package',
-                          rid=config.live_role['auto_rid'],
-                          giftId=self.commodity_id['cid_340'],
-                          money=9900*2,
-                          num=2,
-                          package_cid=cid,
-                          ctype='gift')
-        res = post_request_session(config.pay_url, data)
+        payload = 'platform=available&type=package&money=9900&params=%7B%22rid%22%3A193185484%2C%22uids%22%3A%22105002312%22%2C%22positions%22%3A%220%22%2C%22position%22%3A-1%2C%22giftId%22%3A54%2C%22giftNum%22%3A1%2C%22price%22%3A9900%2C%22cid%22%3A{}%2C%22ctype%22%3A%22gift%22%2C%22duction_money%22%3A0%2C%22version%22%3A2%2C%22num%22%3A1%2C%22gift_type%22%3A%22normal%22%2C%22star%22%3A0%2C%22refer%22%3A%22%E7%83%AD%E9%97%A8%3Aroom%22%2C%22useCoin%22%3A-1%7D'.format(
+            cid)
+        res = post_request_session(url=self.php_urL['pay_url'], data=payload)
         assert_code(res['code'], 200)
         getValue(res)
 
     def endPayCreate(self):
         assert_equal(mysql.checkUserCommoditySql(config.payUid, self.commodity_id['cid_340']), 0)
         sleep(1)
-        assert_equal(mysql.selectAllMoneySql(config.rewardUid), 12276)
+        assert_equal(mysql.selectAllMoneySql(config.rewardUid), 6138)
         assert_equal(Consts.success_num, 1)
         Consts.fail_num = 0
 
@@ -162,6 +159,7 @@ class TestPayConcurrent:
     def main(self, num):
         self.test_01_payCreate(num)
         self.test_02_commodityUse(num)
+        # self.test_03_commodityPresent(num)
         case_list = method.dictToListSlack(Consts.case_list_c)
         des = "{}\n".format(case_list)
         Logs.get_log('concurrentCaseResult.log').info(des)
