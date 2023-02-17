db987b1a1eb0fa35c5a19f07a51863f5fb364f27
li.ximing <li.ximing@olaola.chat>
2023-02-17 15:19:06 +0800

    '分成体系优化'

--- testConcurrent.py
+++ testConcurrent.py
@@ -154,7 +154,7 @@ class TestPayConcurrent:
     def main(self, num):
         self.test_01_payCreate(num)
         self.test_02_commodityUse(num)
-        # self.test_03_commodityPresent(num)
+        self.test_03_commodityPresent(num)
         case_list = method.dictToListSlack(Consts.case_list_c)
         des = "{}\n".format(case_list)
         Logs.get_log('concurrentCaseResult.log').info(des)
