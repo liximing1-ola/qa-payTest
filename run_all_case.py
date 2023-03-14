e9cb11d17d782482361dfd9ef78d1ece28bedefc
li.ximing <li.ximing@olaola.chat>
2023-03-14 11:44:38 +0800

    '分成体系优化'

--- run_all_case.py
+++ run_all_case.py
@@ -130,7 +130,7 @@ def all_case(appInfo):
 
     testcase = unittest.TestSuite()
     discover = unittest.defaultTestLoader.discover(case_dir,  # 指定待执行用例的目录
-                                                   pattern="test_pay_pretty.py",
+                                                   pattern="test_pay_prettyRoom.py",
                                                    top_level_dir=None)
     testcase.addTests(discover)
     return testcase
