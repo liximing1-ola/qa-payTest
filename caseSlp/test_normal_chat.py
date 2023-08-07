a1f16142fd0808bb14e7ded1dcca811ccdcd1058
wzx <Wu.Zhenxing@olaola.chat>
2023-08-07 18:59:24 +0800

    新增slp case

--- caseSlp/test_normal_chat.py
+++ caseSlp/test_normal_chat.py
@@ -5,7 +5,7 @@ __desc__ = "普通用户-私聊"
 
 import unittest
 
-from caseSlp import config
+from caseSlp.config import *
 from common.Assert import assert_code, assert_equal, assert_body
 from common.Consts import case_list, result
 from common.Request import post_request_session
@@ -29,17 +29,17 @@ class TestPayCreate(unittest.TestCase):
 		4.检查预期返回msg，预期：支付失败，提示Toast
 		5.检查被打赏者余额,预期：0
 		"""
-		mysql.updateUserMoneyClearSql(config.payUid, config.normal_uid)
-		# mysql.deleteUserAccountSql('broker_user', config.normal_uid)
-		# mysql.deleteUserAccountSql('chatroom', config.normal_uid)
+		mysql.updateUserMoneyClearSql(payUid, normal_uid)
+		# mysql.deleteUserAccountSql('broker_user', normal_uid)
+		# mysql.deleteUserAccountSql('chatroom', normal_uid)
 		data = encodeData(payType='chat-gift',
-		                  num=config.default_num,
-		                  giftId=config.giftId['5'])
-		res = post_request_session(config.pay_url, data, tokenName='slp')
+		                  num=default_num,
+		                  giftId=giftId['5'])
+		res = post_request_session(pay_url, data, tokenName='slp')
 		assert_code(res['code'])
 		assert_body(res['body'], 'success', 0, reason(des, res))
 		assert_body(res['body'], 'msg', '余额不足，无法支付', reason(des, res))
-		assert_equal(mysql.selectUserInfoSql('sum_money', config.normal_uid), 0)
+		assert_equal(mysql.selectUserInfoSql('sum_money', normal_uid), 0)
 		case_list[des] = result
 
 	def test_02_chatPay(self, des='私聊打赏分成60%进mcb'):
@@ -53,18 +53,18 @@ class TestPayCreate(unittest.TestCase):
         4.检查被打赏者余额和账户，预期为：50 * 0.6 = 30(mcb)
         5.检查打赏者余额.预期为：1000 - 50 = 950
 		"""
-		mysql.updateUserMoneyClearSql(config.payUid, config.default_money)
-		mysql.updateUserMoneyClearSql(config.normal_uid)
-		# mysql.deleteUserAccountSql('broker_user', config.normal_uid)
-		# mysql.deleteUserAccountSql('chatroom', config.normal_uid)
+		mysql.updateUserMoneyClearSql(payUid, default_money)
+		mysql.updateUserMoneyClearSql(normal_uid)
+		# mysql.deleteUserAccountSql('broker_user', normal_uid)
+		# mysql.deleteUserAccountSql('chatroom', normal_uid)
 		data = encodeData(payType='chat-gift',
-		                  num=config.default_num,
-		                  giftId=config.giftId['5'])
-		res = post_request_session(config.pay_url, data, tokenName='slp')
+		                  num=default_num,
+		                  giftId=giftId['5'])
+		res = post_request_session(pay_url, data, tokenName='slp')
 		assert_code(res['code'])
 		assert_body(res['body'], 'success', 1, reason(des, res))
-		assert_equal(mysql.selectUserInfoSql('single_money', config.normal_uid),
-		             config.default_money * config.rates['normal']['default'])
-		assert_equal(mysql.selectUserInfoSql('sum_money', config.payUid),
-		             config.default_money - config.giftId['5'] * config.default_num)
+		assert_equal(mysql.selectUserInfoSql('single_money', normal_uid),
+		             default_money * rates['normal']['default'])
+		assert_equal(mysql.selectUserInfoSql('sum_money', payUid),
+		             default_money - giftId['5'] * default_num)
 		case_list[des] = result
