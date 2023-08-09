b3616dcc2b01ee39bcdf0ac38ca90425c548df18
wzx <Wu.Zhenxing@olaola.chat>
2023-08-09 18:27:04 +0800

    新增slp case

--- caseSlp/test_check.py
+++ caseSlp/test_check.py
@@ -64,3 +64,53 @@ class TestPayCreate(unittest.TestCase):
 		assert_body(res['body'], 'msg', '余额不足，无法支付', reason(des, res))
 		assert_equal(mysql.selectUserInfoSql('sum_money', normal_uid), 0)
 		case_list[des] = result
+
+	def test_03_chatPayNoMoney(self, des='余额<礼物价值-私聊打赏的场景'):
+		"""
+		用例描述：
+		余额<礼物价值-私聊打赏的场景
+		脚本步骤：
+		1.构造打赏者和被打赏者数据
+		2.私聊一对一打赏流程(礼物:棒棒糖)
+		3.校验接口和返回值数据
+		4.检查预期返回msg，预期：支付失败，提示Toast
+		5.检查被打赏者余额,预期：0
+		"""
+		mysql.updateUserMoneyClearSql(payUid, normal_uid)
+		mysql.updateMoneySql(payUid, giftId['5']['price'] - 1)
+		# mysql.deleteUserAccountSql('broker_user', normal_uid)
+		# mysql.deleteUserAccountSql('chatroom', normal_uid)
+		data = encodeData(payType='chat-gift',
+		                  num=default_num,
+		                  giftId=giftId['5']['id'])
+		res = post_request_session(pay_url, data, tokenName='slp')
+		assert_code(res['code'])
+		assert_body(res['body'], 'success', 0, reason(des, res))
+		assert_body(res['body'], 'msg', '余额不足，无法支付', reason(des, res))
+		assert_equal(mysql.selectUserInfoSql('sum_money', normal_uid), 0)
+		case_list[des] = result
+
+	def test_04_roomPayNoMoney(self, des='余余额<礼物价值-房间打赏的场景'):
+		"""
+		用例描述：
+		检查账户余额不足时，房间打赏
+		脚本步骤：
+		1.构造打赏者和被打赏者数据
+		2.私聊一对一打赏流程(礼物:棒棒糖)
+		3.校验接口和返回值数据
+		4.检查预期返回msg，预期：支付失败，提示Toast
+		5.检查被打赏者余额,预期：0
+		"""
+		mysql.updateUserMoneyClearSql(payUid, normal_uid)
+		mysql.updateMoneySql(payUid, giftId['5']['price'] - 1)
+		# mysql.deleteUserAccountSql('broker_user', normal_uid)
+		# mysql.deleteUserAccountSql('chatroom', normal_uid)
+		data = encodeData(payType='package',
+		                  num=default_num,
+		                  giftId=giftId['5']['id'])
+		res = post_request_session(pay_url, data, tokenName='slp')
+		assert_code(res['code'])
+		assert_body(res['body'], 'success', 0, reason(des, res))
+		assert_body(res['body'], 'msg', '余额不足，无法支付', reason(des, res))
+		assert_equal(mysql.selectUserInfoSql('sum_money', normal_uid), 0)
+		case_list[des] = result
