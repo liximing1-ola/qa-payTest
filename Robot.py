d88f337c0d01178418bff4663a6e9be98bc7a823
wzx <Wu.Zhenxing@olaola.chat>
2023-08-16 16:57:33 +0800

    新增slp case,自己打赏自己的case

--- Robot.py
+++ Robot.py
@@ -20,8 +20,8 @@ def robot(mode, reason, title='', bot='BB', color="good"):
         # 'starify': 'https://hooks.slack.com/services/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',  # todo 调试
         'slp': 'https://hooks.slack.com/services/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',  # todo 调试
     }
-    url = robot_dict[bot]
-    # url = robot_dict_wechat[bot]
+    # url = robot_dict[bot]
+    url = robot_dict_wechat[bot]
 
     if mode == 'fail':
         content = "警告! 失败用例: {}, 失败原因: {}".format(title, reason)
--- run_all_case.py
+++ run_all_case.py
@@ -131,19 +131,19 @@ def main(appInfo):
                     len(test_result.failures) + len(test_result.errors),
                     use_time,
                     config.codeInfo['slp_git_branch'])
-                robot('slack', des, bot='slp')
+                robot('markdown', des, bot='slp')
                 sleep(0.1)
-                robot('slack', des_2, bot='slp')
+                robot('markdown', des_2, bot='slp')
             if len(test_result.failures) >= 1:
                 Logs.get_log('failCase.log').error("failures: {}".format(test_result.failures))
-                robot('slack', des, color='danger', bot='slp')
+                robot('fail', des, color='danger', bot='slp')
                 for case, reason in test_result.failures:
-                    robot('slack', Consts.fail_case_reason[0], title=case.id(), color='danger', bot='slp')
+                    robot('fail', Consts.fail_case_reason[0], title=case.id(), color='danger', bot='slp')
                     # break
             if len(test_result.errors) >= 1:
                 Logs.get_log('failCase.log').error("error: {}".format(test_result.errors))
                 for case, reason in test_result.errors:
-                    robot('slack', reason, case.id(), color='danger', bot='slp')
+                    robot('fail', reason, case.id(), color='danger', bot='slp')
                     # break
         else:
             Logs.get_log('runCode.log').info('NoRun')
